899c4354135ed9ea5215707b636918ed93f0b628
Zain Adam <zainadam.id@gmail.com>
2023-05-30 18:53:46 +0700

    chore: implement material ui

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,15 +1,25 @@
-import ApplicationLogo from '@/components/ApplicationLogo'
-import AuthCard from '@/components/AuthCard'
-import AuthSessionStatus from '@/components/AuthSessionStatus'
-import Button from '@/components/Button'
-import GuestLayout from '@/components/Layouts/GuestLayout'
-import Input from '@/components/Input'
-import InputError from '@/components/InputError'
-import Label from '@/components/Label'
-import Link from 'next/link'
-import { useAuth } from '@/hooks/auth'
 import { useEffect, useState } from 'react'
+
+import { useAuth } from '@/hooks/auth'
+
 import { useRouter } from 'next/router'
+
+import Avatar from '@mui/material/Avatar'
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Checkbox from '@mui/material/Checkbox'
+import CircularProgress from '@mui/material/CircularProgress'
+import Container from '@mui/material/Container'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Grid from '@mui/material/Grid'
+import Link from '@mui/material/Link'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined'
+
+import GuestLayout from '@/components/Layouts/GuestLayout'
 
 const Login = () => {
     const router = useRouter()
@@ -23,7 +33,8 @@ const Login = () => {
     const [password, setPassword] = useState('')
     const [shouldRemember, setShouldRemember] = useState(false)
     const [errors, setErrors] = useState([])
-    const [status, setStatus] = useState(null)
+    const [status, setStatus] = useState(null);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         if (router.query.reset?.length > 0 && errors.length === 0) {
@@ -33,8 +44,16 @@ const Login = () => {
         }
     })
 
+    useEffect(() => {
+        if (Object.keys(errors).length > 0) {
+            setLoading(false);
+        }
+    }, [errors])
+
     const submitForm = async event => {
-        event.preventDefault()
+        event.preventDefault();
+
+        setLoading(true);
 
         login({
             email,
@@ -47,86 +66,103 @@ const Login = () => {
 
     return (
         <GuestLayout>
-            <AuthCard
-                logo={
-                    <Link href="/">
-                        <ApplicationLogo className="w-20 h-20 fill-current text-gray-500" />
-                    </Link>
-                }>
-                {/* Session Status */}
-                <AuthSessionStatus className="mb-4" status={status} />
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Login
+                    </Typography>
 
-                <form onSubmit={submitForm}>
-                    {/* Email Address */}
-                    <div>
-                        <Label htmlFor="email">Email</Label>
-
-                        <Input
-                            id="email"
-                            type="email"
-                            value={email}
-                            className="block mt-1 w-full"
-                            onChange={event => setEmail(event.target.value)}
-                            required
-                            autoFocus
-                        />
-
-                        <InputError messages={errors.email} className="mt-2" />
-                    </div>
-
-                    {/* Password */}
-                    <div className="mt-4">
-                        <Label htmlFor="password">Password</Label>
-
-                        <Input
-                            id="password"
-                            type="password"
-                            value={password}
-                            className="block mt-1 w-full"
-                            onChange={event => setPassword(event.target.value)}
-                            required
-                            autoComplete="current-password"
-                        />
-
-                        <InputError
-                            messages={errors.password}
-                            className="mt-2"
-                        />
-                    </div>
-
-                    {/* Remember Me */}
-                    <div className="block mt-4">
-                        <label
-                            htmlFor="remember_me"
-                            className="inline-flex items-center">
-                            <input
-                                id="remember_me"
-                                type="checkbox"
-                                name="remember"
-                                className="rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
-                                onChange={event =>
-                                    setShouldRemember(event.target.checked)
+                    {
+                        loading ?
+                            <Box textAlign='center' mt={4}>
+                                <CircularProgress />
+                            </Box> :
+                            <>
+                                {
+                                    Object.keys(errors).length > 0 && <Paper elevation={3} sx={{ backgroundColor: "#ef5350", color: "#FFF", p: 2, mt: 2, mb: 2, width: '100%', maxWidth: '400px' }}>
+                                        {
+                                            Object.keys(errors).map((key, index) => {
+                                                return (
+                                                    <Typography key={index} variant="body2" align="center">
+                                                        {errors[key]}
+                                                    </Typography>
+                                                )
+                                            })
+                                        }
+                                    </Paper>
                                 }
-                            />
+                                <Box component="form" onSubmit={submitForm} noValidate sx={{ mt: 1 }}>
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                        autoFocus
 
-                            <span className="ml-2 text-sm text-gray-600">
-                                Remember me
-                            </span>
-                        </label>
-                    </div>
+                                        onChange={event => setEmail(event.target.value)}
+                                    />
+                                    <TextField
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="current-password"
 
-                    <div className="flex items-center justify-end mt-4">
-                        <Link
-                            href="/forgot-password"
-                            className="underline text-sm text-gray-600 hover:text-gray-900">
-                            Forgot your password?
-                        </Link>
+                                        onChange={event => setPassword(event.target.value)}
+                                    />
+                                    <FormControlLabel
+                                        control={<Checkbox value="remember" color="primary" />}
+                                        label="Remember me"
+                                        onChange={event => setShouldRemember(event.target.checked)}
+                                    />
 
-                        <Button className="ml-3">Login</Button>
-                    </div>
-                </form>
-            </AuthCard>
+                                    <Button
+                                        type="submit"
+                                        fullWidth
+                                        variant="contained"
+                                        sx={{ mt: 3, mb: 2 }}
+                                    >
+                                        Sign In
+                                    </Button>
+                                    <Grid container>
+                                        <Grid item xs>
+                                            <Link href="#" variant="body2">
+                                                Lupa password?
+                                            </Link>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </>
+                    }
+                </Box>
+
+                <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 8, mb: 4 }}>
+                    {'Copyright © '}
+                    <Link color="inherit" href="https://github.com/sensasi-apps" target='_blank'>
+                        Sensasi Apps
+                    </Link>{' '}
+                    {new Date().getFullYear()}
+                    {'.'}
+                </Typography>
+            </Container>
         </GuestLayout>
+
     )
 }
 
