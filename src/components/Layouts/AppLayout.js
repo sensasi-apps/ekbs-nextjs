9cfd3ca6f1b0a85add4ea12cfd2c73a7794d25a6
Zain Adam <zainadam.id@gmail.com>
2023-06-08 00:22:17 +0700

    chore: set width on xs main el

--- src/components/Layouts/AppLayout.js
+++ src/components/Layouts/AppLayout.js
@@ -23,7 +23,12 @@ const AppLayout = ({ pageTitle, children }) => {
 
             <Box
                 component="main"
-                sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
+                sx={{
+                    flexGrow: 1, p: 3, width: {
+                        xs: '100%',
+                        sm: `calc(100% - ${drawerWidth}px)`,
+                    }
+                }}
             >
                 <Toolbar />
 
