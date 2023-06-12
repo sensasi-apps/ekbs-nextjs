a66eb5b708fd09f959b9f017714d1f35595959fb
Zain Adam <zainadam.id@gmail.com>
2023-06-12 20:12:16 +0700

    chore: add isShow prop

--- src/components/Statuses/LoadingCenter.js
+++ src/components/Statuses/LoadingCenter.js
@@ -3,8 +3,13 @@
 import Box from "@mui/material/Box";
 import CircularProgress from "@mui/material/CircularProgress";
 
-export default function LoadingCenter(props) {
-	return <Box {...props} my={4} textAlign='center'>
+export default function LoadingCenter({ isShow = true, ...props }) {
+	return <Box
+		sx={{
+			display: isShow ? 'block' : 'none'
+		}} my={3} textAlign='center'
+		{...props}
+	>
 		<CircularProgress />
 	</Box>;
 }
