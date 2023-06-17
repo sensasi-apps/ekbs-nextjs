63a7110741ae549da2c791af217ac18d745146e9
Zain Adam <zainadam.id@gmail.com>
2023-06-17 17:11:48 +0800

    chore: welcome on dashboard

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,22 +1,27 @@
-import AppLayout from '@/components/Layouts/AppLayout'
 import Head from 'next/head'
 
+import Box from '@mui/material/Box'
+import Skeleton from '@mui/material/Skeleton'
+import Typography from '@mui/material/Typography'
+
+import AppLayout from '@/components/Layouts/AppLayout'
+import { useAuth } from '@/hooks/auth'
+
 const Dashboard = () => {
+    const { user } = useAuth({ middleware: 'auth' });
+
     return (
         <AppLayout pageTitle='Dasbor'>
             <Head>
-                <title>Laravel - Dashboard</title>
+                <title>{`Dasbor — ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
             </Head>
 
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 bg-white border-b border-gray-200">
-                            You're logged in!
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Box display='flex' gap={1}>
+                <Typography variant="h5" component='span'>Selamat datang,</Typography>
+                <Typography variant="h5" component='span' display='flex'>
+                    <span style={{ color: 'darkturquoise' }}>{user.name || <Skeleton component='span' width="5rem" />}</span>.
+                </Typography>
+            </Box>
         </AppLayout>
     )
 }
