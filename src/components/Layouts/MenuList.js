9a2f71e4730aaf5b6a06eac1fe6d693b0fa73be5
Zain Adam <zainadam.id@gmail.com>
2023-06-18 17:58:50 +0800

    chore: menus based on user logged in

--- src/components/Layouts/MenuList.js
+++ src/components/Layouts/MenuList.js
@@ -1,11 +1,14 @@
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { AppContext } from '../AppContext';
 import { useContext } from 'react';
+import { useAuth } from '@/hooks/auth';
+
+import debounce from '@/lib/debounce';
 
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
@@ -19,80 +22,165 @@ import FireTruckIcon from '@mui/icons-material/FireTruck';
 import CurrencyExchangeIcon from '@mui/icons-material/CurrencyExchange';
 import BalanceIcon from '@mui/icons-material/Balance';
 import ForestIcon from '@mui/icons-material/Forest';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+
+function isAuthorized(user, menu) {
+	if (user?.role_names?.includes('superman') || !menu.forRoles || !menu.forPermissions) {
+		return true;
+	}
+
+	let isAuthorized = false;
+
+	user?.role_names?.forEach(role => {
+		if (menu.forRoles.includes(role)) {
+			isAuthorized = true;
+			return;
+		}
+	});
+
+	if (isAuthorized) {
+		return isAuthorized;
+	}
+
+	user?.permission_names?.forEach(permission => {
+		if (menu.forPermissions.includes(permission)) {
+			isAuthorized = true;
+			return;
+		}
+	});
+
+	return isAuthorized;
+}
+
+function CustomListItem({ data, user, ...props }) {
+	const router = useRouter();
+
+	if (!isAuthorized(user, data)) {
+		return;
+	}
+
+	if (data.component) {
+		return data.component;
+	}
+
+	return <ListItem disablePadding>
+		<ListItemButton
+			shallow={true}
+			passHref
+			href={data.href}
+			selected={router.pathname === data.pathname}
+			{...props}
+		>
+			<ListItemIcon>
+				{data.icon}
+			</ListItemIcon>
+			<ListItemText primary={data.label} />
+		</ListItemButton>
+	</ListItem>
+}
 
 const drawerWidth = 240;
 
-function MenuList(props) {
+const SubTitle = ({ children }) => <Typography ml={2} mt={2} variant='overline' color='grey' fontWeight='bold'>{children}</Typography>
 
-	const router = useRouter();
+const MENUS_DATA = [
+	{
+		href: '/dashboard',
+		label: 'Dasbor',
+		pathname: '/dashboard',
+		icon: <DashboardIcon />
+	}, {
+		component: <SubTitle children='Unit' />
+	}, {
+		href: '/TBS',
+		label: 'TBS',
+		pathname: '/TBS',
+		icon: <BalanceIcon />
+	}, {
+		href: '/saprodi',
+		label: 'SAPRODI',
+		pathname: '/saprodi',
+		icon: <ForestIcon />
+	}, {
+		href: '/alat-berat',
+		label: 'Alat Berat',
+		pathname: '/alat-berat',
+		icon: <FireTruckIcon />
+	}, {
+		href: '/spp',
+		label: 'SPP',
+		pathname: '/spp',
+		icon: <CurrencyExchangeIcon />
+	}, {
+		component: <Divider sx={{ mt: 2 }} />
+	}, {
+		component: <SubTitle children='Keuangan' />
+	}, {
+		href: '/cashes',
+		label: 'Kas',
+		pathname: '/cashes/[[...uuid]]',
+		icon: <AutoStoriesIcon />
+	}, {
+		component: <Divider sx={{ mt: 2 }} />,
+		forRoles: ['users admin'],
+		forPermissions: [
+			'users create',
+			'users update',
+			'users read',
+			'users search'
+		]
+	}, {
+		component: <SubTitle children='Sistem' />,
+		forRoles: ['users admin'],
+		forPermissions: [
+			'users create',
+			'users update',
+			'users read',
+			'users search'
+		]
+	}, {
+		href: '/users',
+		label: 'Pengguna',
+		pathname: '/users/[[...uuid]]',
+		icon: <GroupIcon />,
+		forRoles: ['users admin'],
+		forPermissions: [
+			'users create',
+			'users update',
+			'users read',
+			'users search'
+		]
+	}
+]
+
+function MenuList() {
+
 	const { isDrawerOpen, toggleDrawer } = useContext(AppContext);
+	const [drawerProps, setDrawerProps] = useState({});
 
-	const drawer = (
-		<>
-			<Toolbar />
-			<Divider />
+	const { user } = useAuth({ middleware: 'auth' });
 
-			<ListItem disablePadding sx={{ mt: 2 }}>
-				<ListItemButton href='/dashboard' shallow={true} passHref selected={router.pathname === '/dashboard'}>
-					<ListItemIcon>
-						<DashboardIcon />
-					</ListItemIcon>
-					<ListItemText primary="Dasbor" />
-				</ListItemButton>
-			</ListItem>
-			<Typography ml={2} mt={2} variant='overline' color='grey' fontWeight='bold'>Unit</Typography>
+	function GET_DRAWER_PROPS() {
+		if (window.innerWidth < 600) {
+			return {
+				variant: "temporary",
+				onClose: toggleDrawer
+			}
+		}
 
-			<ListItem disablePadding>
-				<ListItemButton href='/TBS' shallow={true} passHref selected={router.pathname === '/TBS'}>
-					<ListItemIcon>
-						<BalanceIcon />
-					</ListItemIcon>
-					<ListItemText primary="TBS" />
-				</ListItemButton>
-			</ListItem>
+		return {
+			variant: "permanent",
+			onClose: null
+		}
+	}
 
-			<ListItem disablePadding>
-				<ListItemButton href='/saprodi' shallow={true} passHref selected={router.pathname === '/saprodi'}>
-					<ListItemIcon>
-						<ForestIcon />
-					</ListItemIcon>
-					<ListItemText primary="SAPRODI" />
-				</ListItemButton>
-			</ListItem>
+	useEffect(() => {
+		setDrawerProps(GET_DRAWER_PROPS());
 
-			<ListItem disablePadding>
-				<ListItemButton href='/alat-berat' shallow={true} passHref selected={router.pathname === '/alat-berat'}>
-					<ListItemIcon>
-						<FireTruckIcon />
-					</ListItemIcon>
-					<ListItemText primary="Alat Berat" />
-				</ListItemButton>
-			</ListItem>
-
-			<ListItem disablePadding>
-				<ListItemButton href='/spp' shallow={true} passHref selected={router.pathname === '/spp'}>
-					<ListItemIcon>
-						<CurrencyExchangeIcon />
-					</ListItemIcon>
-					<ListItemText primary="SPP" />
-				</ListItemButton>
-			</ListItem>
-
-			<Divider sx={{ mt: 2 }} />
-			<Typography ml={2} mt={2} variant='overline' color='grey' fontWeight='bold'>Sistem</Typography>
-
-			<List>
-				<ListItem disablePadding>
-					<ListItemButton href='/users' shallow={true} passHref selected={router.pathname === '/users/[[...uuid]]'}>
-						<ListItemIcon>
-							<GroupIcon />
-						</ListItemIcon>
-						<ListItemText primary="Pengguna" />
-					</ListItemButton>
-				</ListItem>
-			</List>
-		</>
-	)
+		window.addEventListener("resize", debounce(() => {
+			setDrawerProps(GET_DRAWER_PROPS());
+		}, 300));
+	}, []);
 
 	return (
 		<Box
@@ -100,31 +188,29 @@ function MenuList(props) {
 			sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
 			aria-label="mailbox folders"
 		>
-			{/* The implementation can be swapped with js to avoid SEO duplication of links. */}
 			<Drawer
-				variant="temporary"
+				{...drawerProps}
 				open={isDrawerOpen}
-				onClose={toggleDrawer}
 				ModalProps={{
 					keepMounted: true, // Better open performance on mobile.
 				}}
 				sx={{
-					display: { xs: 'block', sm: 'none' },
-					'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+					'& .MuiDrawer-paper': {
+						boxSizing: 'border-box',
+						width: drawerWidth
+					}
 				}}
 			>
-				{drawer}
-			</Drawer>
+				<Toolbar />
 
-			<Drawer
-				variant="permanent"
-				sx={{
-					display: { xs: 'none', sm: 'block' },
-					'& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-				}}
-				open
-			>
-				{drawer}
+				{
+					MENUS_DATA.map((data, index) => <CustomListItem
+						key={index}
+						data={data}
+						user={user}
+						onClick={debounce(toggleDrawer, 300)}
+					/>)
+				}
 			</Drawer>
 		</Box>
 	);
