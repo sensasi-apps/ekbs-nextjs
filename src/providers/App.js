9906f79bc6d4a292ba4541f443ae44da6c790920
Zain Adam <zainadam.id@gmail.com>
2023-06-28 01:45:44 +0800

    refactor: move hook to context

--- src/providers/App.js
+++ src/providers/App.js
@@ -18,6 +18,16 @@ export const AppProvider = ({ children }) => {
         axios
             .get(url)
             .then(res => {
+                // default from breeze
+                //     if (middleware === 'guest' && redirectIfAuthenticated && user)
+                //         router.push(redirectIfAuthenticated)
+                //     if (
+                //         window.location.pathname === '/verify-email' &&
+                //         user?.email_verified_at
+                //     )
+                //         router.push(redirectIfAuthenticated)
+                //     // if (middleware === 'auth' && error) logout()
+
                 if (!res.data?.is_active) router.push('logout?error=inactive')
                 if (res.data) window.localStorage.setItem('isLoggedIn', true)
 
@@ -62,3 +72,19 @@ export const AppProvider = ({ children }) => {
 }
 
 export default AppContext
+
+// register default from breeze
+// const register = async ({ setErrors, ...props }) => {
+//     // await csrf()
+
+//     setErrors([])
+
+//     axios
+//         .post('/register', props)
+//         // .then(() => mutate())
+//         .catch(error => {
+//             if (error.response.status !== 422) throw error
+
+//             setErrors(error.response.data.errors)
+//         })
+// }
