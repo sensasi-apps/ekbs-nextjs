fd7deeee17c47f02d54fb6abc146d5dbf61821b0
Zain Adam <zainadam.id@gmail.com>
2023-08-21 04:30:35 +0800

    chore: add global components

---
+++ src/components/Global/Dialog/WithUseFormData.tsx
@@ -0,0 +1,34 @@
+import { FC, ReactNode } from 'react'
+import useFormData from '@/providers/useFormData2'
+import Dialog, { DialogProps } from '.'
+
+interface DialogWithUseFormDataProps
+    extends Omit<DialogProps, 'open' | 'closeButtonProps'> {
+    title: string
+    children: ReactNode
+}
+
+const DialogWithUseFormData: FC<DialogWithUseFormDataProps> = ({
+    title,
+    children,
+    ...rest
+}) => {
+    const { formOpen, handleClose, loading } = useFormData()
+
+    const dialogProps = {
+        title: title,
+        open: formOpen,
+        closeButtonProps: {
+            onClick: handleClose,
+            disabled: loading,
+        },
+    }
+
+    return (
+        <Dialog {...dialogProps} {...rest}>
+            {children}
+        </Dialog>
+    )
+}
+
+export default DialogWithUseFormData
