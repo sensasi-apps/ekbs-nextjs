4cb5c1477130111cf6a2cb8255c12df2ecc41ee4
Zain Adam <zainadam.id@gmail.com>
2023-08-21 21:22:07 +0800

    fix: sentry won't send error

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,8 +1,8 @@
 import * as Sentry from '@sentry/nextjs'
 
-if (process.env.NEXT_PUBLIC_VERCEL_ENV) {
+if (process.env.NEXT_PUBLIC_SENTRY_DSN) {
     Sentry.init({
-        dsn: 'https://0abfd6d239ca40fbadc2d0f9606e1e3b@o1289319.ingest.sentry.io/4505351364870144',
+        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
         // Set tracesSampleRate to 1.0 to capture 100%
         // of transactions for performance monitoring.
