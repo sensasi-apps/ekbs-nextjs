bf3e0c552ad439393e11428a4f699bb235996840
Zain Adam <zainadam.id@gmail.com>
2023-09-03 16:49:08 +0800

    fix: can't search on mobile

--- src/components/User/Select.js
+++ src/components/User/Select.js
@@ -1,17 +1,21 @@
 import PropTypes from 'prop-types'
-
 import { useState } from 'react'
-
 import useSWRMutation from 'swr/mutation'
+import axios from '@/lib/axios'
 
 import Autocomplete from '@mui/material/Autocomplete'
 import Box from '@mui/material/Box'
 import CircularProgress from '@mui/material/CircularProgress'
+import IconButton from '@mui/material/IconButton'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
-import axios from '@/lib/axios'
+import SearchIcon from '@mui/icons-material/Search'
+
 import RoleChips from './RoleChips'
+
+const fetchUserOptions = (searchUrl, { arg: searchText }) =>
+    axios.get(`${searchUrl}?query=${searchText}`).then(res => res.data)
 
 const UserSelect = ({
     error,
@@ -25,32 +29,31 @@ const UserSelect = ({
     const [isSearched, setIsSearched] = useState(false)
     const [userOptions, setUserOptions] = useState([])
 
-    const fetchUserOptions = async (searchUrl, { arg: { searchText } }) => {
-        const { data } = await axios.get(`${searchUrl}?query=${searchText}`)
-        return data
-    }
-
     const { trigger, isMutating } = useSWRMutation(
         `/users/search`,
         fetchUserOptions,
         {
-            revalidateOnFocus: false,
-            keepPreviousData: true,
+            revalidate: false,
         },
     )
 
-    const handleKeyDown = async e => {
-        if (e.key === 'Enter' && e.target.value.length >= 3 && !isMutating) {
-            e.preventDefault()
-            setIsSearched(true)
-            const data = await trigger({ searchText: e.target.value })
+    const handleSearch = async () => {
+        if (searchText.length >= 3 && !isMutating) {
+            const data = await trigger(searchText)
             setUserOptions(data)
+            setIsSearched(true)
         }
     }
 
-    const handleKeyUp = e => {
-        if (e.key === 'Enter') return
+    const handleTextFieldKeyDown = e => {
+        if (e.key === 'Enter') {
+            e.preventDefault()
 
+            return handleSearch()
+        }
+    }
+
+    const handleTextFieldChange = e => {
         setIsSearched(false)
         setSearchText(e.target.value)
     }
@@ -58,7 +61,7 @@ const UserSelect = ({
     const getNoOptionsText = () => {
         if (searchText.length < 3) return 'Ketik minimal 3 karakter'
 
-        if (!isSearched) return 'Tekan Enter untuk mencari'
+        if (!isSearched) return 'Tekan Enter/Klik Ikon untuk mencari'
 
         return 'Pengguna tidak ditemukan'
     }
@@ -74,8 +77,6 @@ const UserSelect = ({
             options={userOptions}
             getOptionLabel={user => `#${user.id} - ${user.name}`}
             onChange={props.onChange}
-            onKeyDown={handleKeyDown}
-            onKeyUp={handleKeyUp}
             noOptionsText={getNoOptionsText()}
             loadingText="Memuat..."
             filterOptions={x => x}
@@ -100,6 +101,9 @@ const UserSelect = ({
                     label={label}
                     required={required}
                     {...params}
+                    onChange={handleTextFieldChange}
+                    onKeyDown={handleTextFieldKeyDown}
+                    onBlur={() => setSearchText('')}
                     InputProps={{
                         ...params.InputProps,
                         endAdornment: (
@@ -110,6 +114,21 @@ const UserSelect = ({
                                         size={20}
                                     />
                                 )}
+
+                                {!isMutating &&
+                                    !params.InputProps.endAdornment.props
+                                        .children[0] && (
+                                        <IconButton
+                                            sx={{
+                                                p: 0,
+                                            }}
+                                            disabled={searchText.length < 3}
+                                            onClick={() => {
+                                                handleSearch(searchText)
+                                            }}>
+                                            <SearchIcon />
+                                        </IconButton>
+                                    )}
                                 {params.InputProps.endAdornment}
                             </>
                         ),
