dc9be0111e0897aafffedbc3940120ba5254a104
Zain Adam <zainadam.id@gmail.com>
2023-09-15 03:35:27 +0800

    feat: add TBS performance chart

---
+++ src/components/Loan/Creditur/TbsPerformanceChart.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react'
+
+import { useTheme } from '@mui/material'
+
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from 'recharts'
+
+const TbsPerformanceChart: FC<{
+    data: []
+}> = ({ data }) => {
+    const theme = useTheme()
+
+    return (
+        <ResponsiveContainer>
+            <LineChart data={data}>
+                <XAxis dataKey="monthName" />
+                <YAxis
+                    type="number"
+                    domain={['dataMin', 'dataMax']}
+                    tickFormatter={value =>
+                        new Intl.NumberFormat('id-ID', {
+                            notation: 'compact',
+                            compactDisplay: 'short',
+                        }).format(value)
+                    }
+                />
+                <Tooltip
+                    contentStyle={{
+                        backgroundColor: theme.palette.background.default,
+                    }}
+                    formatter={value =>
+                        value.toLocaleString('id-ID', {
+                            style: 'currency',
+                            currency: 'IDR',
+                            maximumFractionDigits: 0,
+                        })
+                    }
+                />
+                <Legend />
+                <Line
+                    type="monotone"
+                    dataKey="palmBunchesRp"
+                    name="TBS"
+                    stroke="#8884d8"
+                />
+                <Line
+                    type="monotone"
+                    dataKey="palmBunchDeliveriesRp"
+                    name="Pengantaran"
+                    stroke="#82ca9d"
+                />
+            </LineChart>
+        </ResponsiveContainer>
+    )
+}
+
+export default TbsPerformanceChart
