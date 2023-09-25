c97f737364c1fea5e8c2aae97635b87fdbf0d48d
Zain Adam <zainadam.id@gmail.com>
2023-09-25 20:30:04 +0800

    chore: add `formDataDrafts`

--- src/DBConfig.ts
+++ src/DBConfig.ts
@@ -1,4 +1,31 @@
-type objectStores = {
+const DBConfig: DBConfigType = {
+    name: 'ekbsDB',
+    version: 1,
+    objectStores: [
+        {
+            name: 'formDataDrafts',
+            optionalParameters: {
+                keyPath: 'id',
+                autoIncrement: true,
+            },
+            indexes: [
+                {
+                    name: 'modelName',
+                    keyPath: 'modelName',
+                },
+                {
+                    name: 'nameId',
+                    keyPath: ['modelName', 'nameId'],
+                    options: { unique: true },
+                },
+            ],
+        },
+    ],
+}
+
+export default DBConfig
+
+interface objectStores {
     name: string
     optionalParameters?: IDBObjectStoreParameters
     indexes?: {
@@ -8,20 +35,8 @@ type objectStores = {
     }[]
 }
 
-type DBConfigType = {
+interface DBConfigType {
     name: string
     version: number
     objectStores: objectStores[]
 }
-
-const DBConfig: DBConfigType = {
-    name: 'ekbsDB',
-    version: 1,
-    objectStores: [
-        {
-            name: 'user',
-        },
-    ],
-}
-
-export default DBConfig
