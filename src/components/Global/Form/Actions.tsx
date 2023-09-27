93e2697115ef6a8141430c34aa6493f35cebb21c
Zain Adam <zainadam.id@gmail.com>
2023-09-27 21:34:49 +0800

    feat: disable submit button when offline

--- src/components/Global/Form/Actions.tsx
+++ src/components/Global/Form/Actions.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -10,42 +10,64 @@ const FormActions: FC<FormActionsProps> = ({
     disabled,
     onCancel,
     onDelete,
-}) => (
-    <Box
-        display="flex"
-        justifyContent={onDelete ? 'space-between' : 'flex-end'}
-        alignContent="end"
-        mt={2}>
-        {onDelete && (
-            <LoadingButton
-                onClick={onDelete}
-                disabled={submitting || disabled}
-                loading={deleting}
-                color="error">
-                Hapus
-            </LoadingButton>
-        )}
+}) => {
+    const [isOffline, setIsOffline] = useState(false)
 
-        <Box display="flex" gap={1}>
-            <Button
-                disabled={submitting || deleting || disabled}
-                type="reset"
-                color="info"
-                onClick={onCancel}>
-                Batal
-            </Button>
+    const handleOffline = () => {
+        setIsOffline(true)
+    }
 
-            <LoadingButton
-                type="submit"
-                disabled={deleting || disabled}
-                loading={submitting}
-                variant="contained"
-                color="info">
-                Simpan
-            </LoadingButton>
+    const handleOnline = () => {
+        setIsOffline(false)
+    }
+
+    useEffect(() => {
+        window.addEventListener('offline', handleOffline, false)
+        window.addEventListener('online', handleOnline, false)
+
+        return () => {
+            window.removeEventListener('offline', handleOffline, false)
+            window.removeEventListener('online', handleOnline, false)
+        }
+    }, [])
+
+    return (
+        <Box
+            display="flex"
+            justifyContent={onDelete ? 'space-between' : 'flex-end'}
+            alignContent="end"
+            mt={2}>
+            {onDelete && (
+                <LoadingButton
+                    onClick={onDelete}
+                    disabled={submitting || disabled || isOffline}
+                    loading={deleting}
+                    color="error">
+                    Hapus
+                </LoadingButton>
+            )}
+
+            <Box display="flex" gap={1}>
+                <Button
+                    disabled={submitting || deleting || disabled}
+                    type="reset"
+                    color="info"
+                    onClick={onCancel}>
+                    Batal
+                </Button>
+
+                <LoadingButton
+                    type="submit"
+                    disabled={deleting || disabled || isOffline}
+                    loading={submitting}
+                    variant={isOffline ? 'outlined' : 'contained'}
+                    color="info">
+                    Simpan
+                </LoadingButton>
+            </Box>
         </Box>
-    </Box>
-)
+    )
+}
 
 export default FormActions
 
