e716b777fea73d2b441a9255f83f29c435f12483
Zain Adam <zainadam.id@gmail.com>
2023-09-28 19:06:56 +0800

    chore: add `HTMLTextAreaElement`

--- src/hooks/useValidationErrors.ts
+++ src/hooks/useValidationErrors.ts
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-
 import type ValidationErrorsType from '@/types/ValidationErrors'
+import type { ChangeEvent } from 'react'
+
+import { useState } from 'react'
 
 const useValidationErrors = () => {
     const [validationErrors, setValidationErrors] =
         useState<ValidationErrorsType>({})
 
-    const clearByEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const clearByEvent = (
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
         const name = event.target.name
 
         if (validationErrors[name]) {
