c74d42740b62f0a486ccd35c991f1d0d1d274777
Zain Adam <zainadam.id@gmail.com>
2023-09-28 17:34:16 +0800

    chore: remove unused

--- src/pages/palm-bunches/delivery-rates.tsx
+++ src/pages/palm-bunches/delivery-rates.tsx
@@ -170,7 +170,6 @@ const Crud = () => {
                     }
                 />
             </Dialog>
-            div
             <Fab
                 disabled={formOpen}
                 onClick={handleCreate}
