5e0d018f687aed6d65d14cd0106d2d5d1eaded14
Zain Adam <zainadam.id@gmail.com>
2023-10-05 09:40:59 +0800

    chore: cache `/api/user`

--- next.config.js
+++ next.config.js
@@ -1,8 +1,20 @@
 // eslint-disable-next-line
+const runtimeCaching = require('next-pwa/cache')
+// eslint-disable-next-line
 const withPWA = require('next-pwa')({
     dest: 'public',
     skipWaiting: true,
     disable: process.env.NODE_ENV === 'development',
+    reloadOnOnline: false,
+    runtimeCaching: [
+        {
+            urlPattern: new RegExp(
+                `^${process.env.NEXT_PUBLIC_BACKEND_URL}/api/user`,
+            ),
+            handler: 'Fastest',
+        },
+        ...runtimeCaching,
+    ],
 })
 
 /**
