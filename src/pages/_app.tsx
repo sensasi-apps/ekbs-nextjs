a7e958c1abbd7dd564cddcf39a8dd3f48be52efd
Zain Adam <zainadam.id@gmail.com>
2023-10-16 21:04:06 +0800

    chore: add roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 import type { AppProps } from 'next/app'
 import type { SnackbarKey } from 'notistack'
 
