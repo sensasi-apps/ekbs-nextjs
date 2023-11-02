70f008d3f9eda151eea9e9b5335be4ca3c8d0ef6
Zain Adam <zainadam.id@gmail.com>
2023-11-02 22:47:08 +0800

    chore: refine 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,65 @@
-const NotFoundPage = () => (
-    <div className="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
-        <div className="max-w-xl mx-auto sm:px-6 lg:px-8">
-            <div className="flex items-center pt-8 sm:justify-start sm:pt-0">
-                <div className="px-4 text-lg text-gray-500 border-r border-gray-400 tracking-wider">
-                    404
-                </div>
+import Head from 'next/head'
+import Link from 'next/link'
+// materials
+import Button from '@mui/material/Button'
+import Container from '@mui/material/Container'
+// icons
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import HelpOutlineIcon from '@mui/icons-material/HelpOutline'
+// etc
+import useAuth from '@/providers/Auth'
+import AuthLayout from '@/components/Layouts/AuthLayout'
+import Footer from '@/components/Layouts/Footer'
 
-                <div className="ml-4 text-lg text-gray-500 uppercase tracking-wider">
-                    Not Found
-                </div>
+export default function NotFoundPage() {
+    const { isAuthenticated } = useAuth()
+
+    const mainContent = (
+        <Container
+            style={{
+                minHeight: isAuthenticated ? 'unset' : '100vh',
+                paddingTop: isAuthenticated ? '3rem' : 'unset',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+            }}>
+            <Head>
+                <title>{`Halaman tidak ditemukan — ${process.env.NEXT_PUBLIC_APP_NAME}`}</title>
+            </Head>
+
+            <div>
+                <HelpOutlineIcon style={{ fontSize: 150 }} color="warning" />
             </div>
-        </div>
-    </div>
-)
 
-export default NotFoundPage
+            <div>
+                <h1>
+                    <div>404</div>
+                    <div>Halaman tidak ditemukan</div>
+                </h1>
+                <p>Sayang sekali, halaman yang anda tuju tidak ditemukan.</p>
+
+                {isAuthenticated ? (
+                    <p>Silakan memilih menu yang tersedia di sebelah kiri.</p>
+                ) : (
+                    <Button
+                        variant="outlined"
+                        size="small"
+                        href="/login"
+                        LinkComponent={Link}
+                        startIcon={<ArrowBackIcon />}>
+                        Kembali ke halaman Login
+                    </Button>
+                )}
+            </div>
+
+            <Footer />
+        </Container>
+    )
+
+    return isAuthenticated ? (
+        <AuthLayout title="Halaman tidak ditemukan">{mainContent}</AuthLayout>
+    ) : (
+        mainContent
+    )
+}
