8ee57a658af9a4f4aab0efb516976b21ee0ca352
Zain Adam <zainadam.id@gmail.com>
2023-11-14 02:18:34 +0800

    refactor: remove `Global` dir

--- src/components/Fab/Fab.tsx
+++ src/components/Fab/Fab.tsx
@@ -1,5 +1,13 @@
 import MuiFab, { FabProps } from '@mui/material/Fab'
 
+/**
+ * A floating action button component.
+ * @default
+ * - color: success
+ * - sx: { position: 'fixed', bottom: 16, right: 16 }
+ * @param {FabProps} props - The props for the component.
+ * @returns \@mui/material/Fab.
+ */
 export default function Fab(props: FabProps) {
     return (
         <MuiFab
