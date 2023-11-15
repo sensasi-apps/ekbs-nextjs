b67019675048f9209b95ae8d0eeb7c26e27e39b8
Zain Adam <zainadam.id@gmail.com>
2023-11-15 18:52:50 +0800

    chore: optimize `/user-loans` page

---
+++ src/components/pages/user-loans/Form/types.ts
@@ -0,0 +1,15 @@
+import type UserLoanType from '@/dataTypes/Loan'
+
+/**
+ * Represents the data type for the user loan form.
+ */
+export type UserLoanFormDataType = {
+    user_uuid: UserLoanType['user_uuid'] | ''
+    proposed_rp: UserLoanType['proposed_rp'] | ''
+    n_term: UserLoanType['n_term'] | ''
+    term_unit: UserLoanType['term_unit']
+    purpose: UserLoanType['purpose'] | ''
+    proposed_at: UserLoanType['proposed_at'] | null
+    interest_percent: UserLoanType['interest_percent'] | ''
+    type: UserLoanType['type'] | ''
+}
