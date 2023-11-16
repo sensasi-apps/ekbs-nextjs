f7526769e42171df179fa0cb8b84cbc506429be1
Zain Adam <zainadam.id@gmail.com>
2023-11-17 04:15:53 +0800

    chore: remove unused

--- src/lib/numberFormat.js
+++
@@ -1,9 +0,0 @@
-export default function numberFormat(number, options = {}) {
-    const { locale = 'id-ID', currency = 'IDR' } = options
-
-    return new Intl.NumberFormat(locale, {
-        style: 'currency',
-        minimumFractionDigits: 0,
-        currency,
-    }).format(number)
-}
