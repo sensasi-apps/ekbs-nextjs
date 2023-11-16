fe17a80a1b520b96d6f343c086cd1fb08135688d
Zain Adam <zainadam.id@gmail.com>
2023-11-17 04:07:22 +0800

    chore: refine `/user-loans/installments` page

--- src/pages/user-loans/installments.tsx
+++ src/pages/user-loans/installments.tsx
@@ -1,10 +1,71 @@
+// types
+import type {
+    InstallmentUserLoanType,
+    InstallmentWithTransactionType,
+} from '@/dataTypes/Installment'
+// vendors
+import { Formik } from 'formik'
+import { useState } from 'react'
+// components
 import AuthLayout from '@/components/Layouts/AuthLayout'
-import LoansInstallments from '@/components/Loans/Installments'
+import DialogWithTitle from '@/components/DialogWithTitle'
+import { mutate } from '@/components/Datatable'
+// local components
+import UserLoanInstallmentDatatable from '@/components/pages/user-loans/installments/Datatable'
+import UserLoanInstallmentForm, {
+    UserLoanInstallmentFormInitialValues,
+} from '@/components/pages/user-loans/installments/Form'
+import axios from '@/lib/axios'
+// utils
+import errorCatcher from '@/utils/errorCatcher'
 
 export default function UserLoansInstallments() {
+    const [formData, setFormData] = useState(
+        UserLoanInstallmentFormInitialValues,
+    )
+    const [userLoanInstallment, setUserLoanInstallment] = useState<
+        (InstallmentUserLoanType & InstallmentWithTransactionType) | null
+    >(null)
+    const [isOpenDialog, setIsOpenDialog] = useState(false)
+
+    const handleClose = () => setIsOpenDialog(false)
+
+    const handleEdit = (
+        data: InstallmentUserLoanType & InstallmentWithTransactionType,
+    ) => {
+        setFormData({
+            cashable_cash_uuid: data?.transaction?.cashable_uuid ?? '',
+        })
+        setUserLoanInstallment(data)
+        setIsOpenDialog(true)
+    }
+
     return (
         <AuthLayout title="Pembayaran Angsuran">
-            <LoansInstallments />
+            <UserLoanInstallmentDatatable onEdit={handleEdit} />
+
+            <DialogWithTitle title="Pembayaran Angsuran" open={isOpenDialog}>
+                <Formik
+                    initialValues={formData}
+                    initialStatus={{
+                        userLoanInstallment: userLoanInstallment,
+                    }}
+                    onSubmit={(values, { setErrors }) =>
+                        axios
+                            .post(
+                                `user-loans/${userLoanInstallment?.uuid}/installments/collect`,
+                                values,
+                            )
+                            .then(() => {
+                                mutate()
+                                handleClose()
+                            })
+                            .catch(error => errorCatcher(error, setErrors))
+                    }
+                    onReset={handleClose}
+                    component={UserLoanInstallmentForm}
+                />
+            </DialogWithTitle>
         </AuthLayout>
     )
 }
