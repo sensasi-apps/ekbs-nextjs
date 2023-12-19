6a18911b55bae76187ae6dbb8d5db64dfd9aa7b1
Zain Adam <zainadam.id@gmail.com>
2023-12-20 02:19:38 +0800

    chore: add `in` prop with transition

--- src/components/Fab/Fab.tsx
+++ src/components/Fab/Fab.tsx
@@ -1,23 +1,34 @@
-import MuiFab, { FabProps } from '@mui/material/Fab'
+import type { FabProps } from '@mui/material/Fab'
+
+import MuiFab from '@mui/material/Fab'
+import Zoom from '@mui/material/Zoom'
 
 /**
  * A floating action button component.
  * @default
+ * - in: true
  * - color: success
  * - sx: { position: 'fixed', bottom: 16, right: 16 }
  * @param {FabProps} props - The props for the component.
  * @returns \@mui/material/Fab.
  */
-export default function Fab(props: FabProps) {
+export default function Fab({
+    in: inProp = true,
+    ...props
+}: FabProps & {
+    in?: boolean
+}) {
     return (
-        <MuiFab
-            color="success"
-            sx={{
-                position: 'fixed',
-                bottom: 16,
-                right: 16,
-            }}
-            {...props}
-        />
+        <Zoom in={inProp} unmountOnExit>
+            <MuiFab
+                color="success"
+                sx={{
+                    position: 'fixed',
+                    bottom: 16,
+                    right: 16,
+                }}
+                {...props}
+            />
+        </Zoom>
     )
 }
