3bf599003b6187cd57a1b01bcda765121486167c
Zain Adam <zainadam.id@gmail.com>
2023-12-20 20:05:11 +0800

    feat: acting as

---
+++ src/pages/acting-as.tsx
@@ -0,0 +1,27 @@
+// vendors
+import { useState } from 'react'
+// components
+import AuthLayout from '@/components/Layouts/AuthLayout'
+import UserAutocomplete from '@/components/Global/UserAutocomplete'
+import axios from '@/lib/axios'
+
+export default function ActingAs() {
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <AuthLayout title="">
+            <UserAutocomplete
+                disabled={loading}
+                onChange={(_, user) => {
+                    if (!user) return
+
+                    setLoading(true)
+                    return axios
+                        .post(`/acting-asx/${user.uuid}`)
+                        .then(() => (window.location.href = '/dashboard'))
+                        .catch(() => setLoading(false))
+                }}
+            />
+        </AuthLayout>
+    )
+}
