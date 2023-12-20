caed6092a124737843090e7a65edf2adee58110e
Zain Adam <zainadam.id@gmail.com>
2023-12-20 20:03:32 +0800

    refactor: separate

---
+++ src/lib/axios/handleServerError.ts
@@ -0,0 +1,36 @@
+import type { AxiosResponse } from 'axios'
+import { OptionsObject, enqueueSnackbar } from 'notistack'
+
+const SNACKBAR_OPTIONS: OptionsObject = {
+    variant: 'error',
+    persist: true,
+}
+
+export default function handleServerError({ status, data }: AxiosResponse) {
+    switch (status) {
+        case 401:
+            dispatchEvent(new CustomEvent('401Error'))
+            break
+
+        case 403:
+            enqueueSnackbar(
+                'Anda tidak memiliki akses untuk halaman ini',
+                SNACKBAR_OPTIONS,
+            )
+            break
+
+        case 500:
+            enqueueSnackbar(
+                'Server bermasalah, silahkan menghubungi pengurus',
+                SNACKBAR_OPTIONS,
+            )
+            break
+
+        default:
+            enqueueSnackbar(
+                data.message ?? 'Terjadi kesalahan, silahkan coba lagi',
+                SNACKBAR_OPTIONS,
+            )
+            break
+    }
+}
