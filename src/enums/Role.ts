a1c31fa55da6c3f71789cfcc6a75a679aefbd4b8
Zain Adam <zainadam.id@gmail.com>
2023-12-28 01:09:32 +0800

    chore: role enum

---
+++ src/enums/Role.ts
@@ -0,0 +1,47 @@
+/**
+ * *_MANAGER
+ *
+ * *_ADMIN
+ */
+enum Role {
+    // general
+    COURIER = 'courier',
+    DRIVER = 'driver',
+    EMPLOYEE = 'employee',
+    EXECUTIVE = 'executive',
+    FARMER = 'farmer',
+    MEMBER = 'member',
+
+    // farm input
+    FARM_INPUT_MANAGER = 'farm inputs manager',
+    FARM_INPUT_WAREHOUSE_MANAGER = 'farm input warehouse manager',
+    FARM_INPUT_SALES = 'farm input sales',
+    FARM_INPUT_PURCHASER = 'farm inputs purchaser',
+
+    // cash
+    CASH_MANAGER = 'cashes manager',
+    TRANSACTION_MANAGER = 'transactions manager',
+
+    // heavy equipment rent
+    HEAVY_EQUIPMENT_RENT_ADMIN = 'heavy equipment rent admin',
+
+    // inventory
+    INVENTORY_ITEM_MANAGER = 'inventory item manager',
+
+    // loan
+    USER_LOAN_MANAGER = 'user loans manager',
+    USER_LOAN_REVIEWER = 'user loans reviewer',
+    USER_LOAN_DISBURSER = 'user loans disburser',
+    USER_LOAN_INSTALLMENT_COLLECTOR = 'user loan installments collector',
+
+    // palm bunch
+    PALM_BUNCH_ADMIN = 'palm bunches admin',
+    PALM_BUNCH_MANAGER = 'palm bunch head',
+
+    // system
+    USER_ADMIN = 'users admin',
+    SYSTEM_CONFIGURATOR = 'system configurator',
+    SUPERMAN = 'superman',
+}
+
+export default Role
