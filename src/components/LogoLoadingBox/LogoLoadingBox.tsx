5367fada46b1172a9f70577652857934c2fb9a89
Zain Adam <zainadam.id@gmail.com>
2023-12-30 02:27:38 +0800

    refactor: splt unused loading with logo

---
+++ src/components/LogoLoadingBox/LogoLoadingBox.tsx
@@ -0,0 +1,40 @@
+import Box, { BoxProps } from '@mui/material/Box'
+import Image from 'next/image'
+import CircularProgress from '@mui/material/CircularProgress'
+
+const LogoLoadingBox = ({
+    display = 'flex',
+    justifyContent = 'center',
+    alignItems = 'center',
+    height = '100vh',
+    ...restProps
+}: BoxProps) => (
+    <Box
+        display={display}
+        justifyContent={justifyContent}
+        alignItems={alignItems}
+        height={height}
+        {...restProps}>
+        <Image
+            src="/assets/pwa-icons/green-transparent.svg"
+            width={150}
+            height={150}
+            alt="logo"
+            priority
+            style={{
+                position: 'absolute',
+            }}
+        />
+
+        <CircularProgress
+            size={200}
+            thickness={1}
+            color="success"
+            style={{
+                position: 'absolute',
+            }}
+        />
+    </Box>
+)
+
+export default LogoLoadingBox
