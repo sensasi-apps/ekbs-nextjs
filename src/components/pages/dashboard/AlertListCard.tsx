2a9548e909ccb0f9afaf7b50e514bcb8aa91d9e4
Zain Adam <zainadam.id@gmail.com>
2023-12-30 02:35:21 +0800

    WIP: dashboard alert

---
+++ src/components/pages/dashboard/AlertListCard.tsx
@@ -0,0 +1,44 @@
+// vendors
+// import Link from 'next/link'
+//materials
+// import Button from '@mui/material/Button'
+// import Alert from '@mui/material/Alert'
+// import Box from '@mui/material/Box'
+// import Typography from '@mui/material/Typography'
+
+export default function AlertListCard() {
+    return (
+        <>
+            {/* <Typography component="div" mb={2}>
+                Untuk Anda
+            </Typography>
+
+            <Box display="flex" flexDirection="column" gap={1}>
+                <Alert>Belum ada pemberitahuan untuk saat ini</Alert>
+
+                <Alert
+                    severity="warning"
+                    action={
+                        <Button color="inherit" size="small">
+                            Lihat
+                        </Button>
+                    }>
+                    45 data tiket tidak sesuai
+                </Alert>
+                <Alert
+                    severity="warning"
+                    action={
+                        <Button
+                            href="palm-bunches/rea-payments"
+                            color="inherit"
+                            size="small"
+                            LinkComponent={Link}>
+                            Lihat
+                        </Button>
+                    }>
+                    6 data tiket tidak ditemukan
+                </Alert>
+            </Box> */}
+        </>
+    )
+}
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,45 +1,16 @@
-// vendors
-import { useEffect, useState } from 'react'
 // materials
 import Box from '@mui/material/Box'
-import FormControl from '@mui/material/FormControl'
 import Grid2 from '@mui/material/Unstable_Grid2'
-import InputLabel from '@mui/material/InputLabel'
-import Select from '@mui/material/Select'
 import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
-import MenuItem from '@mui/material/MenuItem'
 // components
 import AuthLayout from '@/components/Layouts/AuthLayout'
 import useAuth from '@/providers/Auth'
-// page components
-import TbsWeightCard from '@/components/pages/dashboard/TbsWeightCard'
-import useSWR from 'swr'
-import LoadingCenter from '@/components/Statuses/LoadingCenter'
 // enums
-import DataFreq from '@/enums/DataFreq'
+import AlertListCard from '@/components/pages/dashboard/AlertListCard'
 
 export default function Dashboard() {
     const { user } = useAuth()
-
-    const [dataFreq, setDataFreq] = useState<DataFreq | undefined>()
-
-    useEffect(() => {
-        const dataFreq = localStorage.getItem('dashboard-data-freq') as DataFreq
-
-        setDataFreq(dataFreq ?? 'monthly')
-    }, [])
-
-    const { data = [], isLoading } = useSWR<CardData[]>(
-        dataFreq
-            ? [
-                  '/dashboard/data',
-                  {
-                      freq: dataFreq,
-                  },
-              ]
-            : null,
-    )
 
     return (
         <AuthLayout title="Dasbor">
@@ -62,61 +33,11 @@ export default function Dashboard() {
                 )}
             </Box>
 
-            {data.length > 0 && (
-                <Box mb={2}>
-                    <FormControl
-                        sx={{
-                            minWidth: '8em',
-                        }}>
-                        <InputLabel id="freq-select-label">Tampilan</InputLabel>
-                        <Select
-                            label="Tampilan"
-                            labelId="freq-select-label"
-                            id="freq-select"
-                            size="small"
-                            value={dataFreq ?? ''}
-                            onChange={({ target: { value } }) => {
-                                localStorage.setItem(
-                                    'dashboard-data-freq',
-                                    value as string,
-                                )
-                                setDataFreq(value as DataFreq)
-                            }}>
-                            <MenuItem value="daily">Harian</MenuItem>
-                            <MenuItem value="monthly">Bulanan</MenuItem>
-                            <MenuItem value="yearly">Tahunan</MenuItem>
-                        </Select>
-                    </FormControl>
-                </Box>
-            )}
-
-            <LoadingCenter isShow={isLoading} />
-
             <Grid2 container spacing={2}>
-                {data?.map(getCard)}
+                <Grid2 xs={12} md={4}>
+                    <AlertListCard />
+                </Grid2>
             </Grid2>
         </AuthLayout>
     )
-}
-
-type CardData = {
-    name: CardName
-    freq: DataFreq
-    data: any[]
-}
-
-enum CardName {
-    TBS_WEIGHT = 'TbsWeight',
-}
-
-function getCard({ name, data, freq }: CardData) {
-    switch (name) {
-        case CardName.TBS_WEIGHT:
-            return (
-                <Grid2 key={name} xs={12} md={8}>
-                    <TbsWeightCard data={data} freq={freq} />
-                </Grid2>
-            )
-            break
-    }
 }
