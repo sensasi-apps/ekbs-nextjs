49ddebfeb65a53bfa15f155f43b8357dd378a304
Zain Adam <zainadam.id@gmail.com>
2024-02-08 19:16:36 +0800

    chore: initial `BigNumber` component

---
+++ src/components/StatCard/BigNumber.tsx
@@ -0,0 +1,30 @@
+import type { ReactNode } from 'react'
+import Typography from '@mui/material/Typography'
+import StatCard, { StatCardProps } from './StatCard'
+
+export default function BigNumber({
+    primary,
+    secondary,
+    ...props
+}: Omit<StatCardProps, 'children'> & {
+    children?: never
+    primary: number | string
+    secondary?: number | string | ReactNode
+}) {
+    return (
+        <StatCard {...props}>
+            <Typography variant="h2" component="div">
+                {primary}
+            </Typography>
+
+            {secondary && (
+                <Typography
+                    variant="subtitle2"
+                    component="div"
+                    color="GrayText">
+                    {secondary}
+                </Typography>
+            )}
+        </StatCard>
+    )
+}
