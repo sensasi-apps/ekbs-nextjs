3d38c50e66a26bf34c5652082e50eca6c68dd208
Zain Adam <zainadam.id@gmail.com>
2024-02-11 03:42:24 +0800

    chore: separate negative value

--- src/components/pages/cashes/Cash/AllCashChart.tsx
+++ src/components/pages/cashes/Cash/AllCashChart.tsx
@@ -7,6 +7,7 @@ import useSWR from 'swr'
 import Button, { ButtonProps } from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import Divider from '@mui/material/Divider'
+import Grid2 from '@mui/material/Unstable_Grid2'
 import Tooltip from '@mui/material/Tooltip'
 import Typography from '@mui/material/Typography'
 // components
@@ -33,10 +34,39 @@ export default function AllCashChart({
     >('cashes/all-wallet-balances-summary-data')
 
     const totalCash =
-        cashData?.reduce((acc, curr) => acc + curr.balance, 0) ?? 0.001
+        cashData?.reduce(
+            (acc, curr) => acc + (curr.balance < 0 ? 0 : curr.balance),
+            0,
+        ) ?? 0.001
+
+    const totalNegativeCash =
+        cashData?.reduce(
+            (acc, curr) => acc + (curr.balance < 0 ? curr.balance : 0),
+            0,
+        ) ?? 0.001
+
     const totalWallet =
-        walletData?.reduce((acc, curr) => acc + curr.value, 0) ?? 0.001
-    const max = Math.max(totalCash, totalWallet)
+        walletData?.reduce(
+            (acc, curr) => acc + (curr.value < 0 ? 0 : curr.value),
+            0,
+        ) ?? 0.001
+
+    const totalNegativeWallet =
+        walletData?.reduce(
+            (acc, curr) => acc + (curr.value < 0 ? curr.value : 0),
+            0,
+        ) ?? 0.001
+
+    const totalAbsCash = totalCash + Math.abs(totalNegativeCash)
+    const totalAbsWallet = totalWallet + Math.abs(totalNegativeWallet)
+
+    const maxPositive = Math.max(totalCash, totalWallet)
+    const maxNegative = Math.max(
+        Math.abs(totalNegativeCash),
+        Math.abs(totalNegativeWallet),
+    )
+
+    const max = Math.max(totalAbsCash, totalAbsWallet)
 
     return (
         <StatCard
@@ -44,62 +74,201 @@ export default function AllCashChart({
             isLoading={cashLoading || walletLoading}
             {...props}>
             <Box minWidth={MIN_WIDTH}>
-                <Box width={`${(totalCash / max) * 100}%`}>
-                    <Box display="flex" flexDirection="row" gap={0.5}>
-                        {cashData?.map(({ uuid, name, balance }) => (
-                            <ItemBar
-                                key={uuid}
-                                title={`${name}: ${numberToCurrency(balance)}`}
-                                color={balance >= 0 ? 'success' : 'error'}
-                                sx={{
-                                    width: `${(balance / totalWallet) * 100}%`,
-                                }}>
-                                <Box>{name}</Box>
-                                <Box>
-                                    {numberToCurrency(balance, {
-                                        notation: 'compact',
-                                    })}
-                                </Box>
-                            </ItemBar>
-                        ))}
-                    </Box>
-                    <Divider>
-                        <Typography variant="caption" fontWeight="bold">
-                            {numberToCurrency(totalCash)}
-                        </Typography>
-                    </Divider>
-                </Box>
-            </Box>
+                <Grid2 container spacing={0.5}>
+                    <Grid2 container xs={(maxPositive / max) * 12}>
+                        <Grid2 xs={(totalCash / maxPositive) * 12}>
+                            <Box display="flex" gap={0.5}>
+                                {cashData?.map(({ uuid, name, balance }) => (
+                                    <ItemBar
+                                        key={uuid}
+                                        title={`${name}: ${numberToCurrency(
+                                            balance,
+                                        )}`}
+                                        color={
+                                            balance < 0 ? 'error' : 'success'
+                                        }
+                                        sx={{
+                                            width: `${
+                                                (balance / totalCash) * 100
+                                            }%`,
+                                        }}>
+                                        <Box>{name}</Box>
+                                        <Box>
+                                            {numberToCurrency(balance, {
+                                                notation: 'compact',
+                                            })}
+                                        </Box>
+                                    </ItemBar>
+                                ))}
+                            </Box>
 
-            <Box minWidth={MIN_WIDTH}>
-                <Box width={`${(totalWallet / max) * 100}%`}>
-                    <Box display="flex" gap={0.5}>
-                        {walletData?.map(({ value, label }, index) => (
-                            <ItemBar
-                                key={index}
-                                title={`${label}: ${numberToCurrency(value)}`}
-                                color={value >= 0 ? 'success' : 'error'}
-                                sx={{
-                                    width: `${
-                                        (Math.abs(value) / totalWallet) * 100
-                                    }%`,
-                                }}>
-                                <Box>{label}</Box>
-                                <Box>
-                                    {numberToCurrency(value, {
-                                        notation: 'compact',
-                                    })}
-                                </Box>
-                            </ItemBar>
-                        ))}
-                    </Box>
+                            <Divider>
+                                <Typography variant="caption" fontWeight="bold">
+                                    {numberToCurrency(totalCash)}
+                                </Typography>
+                            </Divider>
+                        </Grid2>
 
-                    <Divider>
-                        <Typography variant="caption" fontWeight="bold">
-                            {numberToCurrency(totalWallet)}
-                        </Typography>
-                    </Divider>
-                </Box>
+                        <Grid2 xs={(totalWallet / maxPositive) * 12}>
+                            <Box display="flex" gap={0.5}>
+                                {walletData?.map(({ value, label }, index) =>
+                                    value < 0 ? null : (
+                                        <ItemBar
+                                            key={index}
+                                            title={`${label}: ${numberToCurrency(
+                                                value,
+                                            )}`}
+                                            color={
+                                                value < 0 ? 'error' : 'success'
+                                            }
+                                            sx={{
+                                                width: `${
+                                                    (Math.abs(value) /
+                                                        totalWallet) *
+                                                    100
+                                                }%`,
+                                            }}>
+                                            <Box>{label}</Box>
+                                            <Box>
+                                                {numberToCurrency(value, {
+                                                    notation: 'compact',
+                                                })}
+                                            </Box>
+                                        </ItemBar>
+                                    ),
+                                )}
+                            </Box>
+
+                            <Divider>
+                                <Typography variant="caption" fontWeight="bold">
+                                    {numberToCurrency(totalWallet)}
+                                </Typography>
+                            </Divider>
+                        </Grid2>
+                    </Grid2>
+
+                    <Grid2 container xs={(maxNegative / max) * 12}>
+                        <Grid2
+                            xs={
+                                (Math.abs(totalNegativeCash) / maxNegative) * 12
+                            }
+                            height="50%">
+                            {totalNegativeCash < 0 && (
+                                <>
+                                    <Box
+                                        display="flex"
+                                        flexDirection="row"
+                                        gap={0.5}>
+                                        {cashData?.map(
+                                            ({ uuid, name, balance }) => (
+                                                <ItemBar
+                                                    key={uuid}
+                                                    title={`${name}: ${numberToCurrency(
+                                                        balance,
+                                                    )}`}
+                                                    color={
+                                                        balance < 0
+                                                            ? 'error'
+                                                            : 'success'
+                                                    }
+                                                    sx={{
+                                                        width: `${
+                                                            (Math.abs(balance) /
+                                                                Math.abs(
+                                                                    totalNegativeCash,
+                                                                )) *
+                                                            100
+                                                        }%`,
+                                                    }}>
+                                                    <Box>{name}</Box>
+                                                    <Box>
+                                                        {numberToCurrency(
+                                                            balance,
+                                                            {
+                                                                notation:
+                                                                    'compact',
+                                                            },
+                                                        )}
+                                                    </Box>
+                                                </ItemBar>
+                                            ),
+                                        )}
+                                    </Box>
+                                    <Divider>
+                                        <Typography
+                                            variant="caption"
+                                            fontWeight="bold">
+                                            {numberToCurrency(
+                                                totalNegativeCash,
+                                            )}
+                                        </Typography>
+                                    </Divider>
+                                </>
+                            )}
+                        </Grid2>
+
+                        <Grid2
+                            xs={
+                                (Math.abs(totalNegativeWallet) / maxNegative) *
+                                12
+                            }
+                            height="50%">
+                            {totalNegativeWallet < 0 && (
+                                <>
+                                    <Box display="flex" gap={0.5}>
+                                        {walletData?.map(
+                                            ({ value, label }, index) =>
+                                                value < 0 ? (
+                                                    <ItemBar
+                                                        key={index}
+                                                        title={`${label}: ${numberToCurrency(
+                                                            value,
+                                                        )}`}
+                                                        color={
+                                                            value < 0
+                                                                ? 'error'
+                                                                : 'success'
+                                                        }
+                                                        sx={{
+                                                            width: `${
+                                                                (Math.abs(
+                                                                    value,
+                                                                ) /
+                                                                    Math.abs(
+                                                                        totalNegativeWallet,
+                                                                    )) *
+                                                                100
+                                                            }%`,
+                                                        }}>
+                                                        <Box>{label}</Box>
+                                                        <Box>
+                                                            {numberToCurrency(
+                                                                value,
+                                                                {
+                                                                    notation:
+                                                                        'compact',
+                                                                },
+                                                            )}
+                                                        </Box>
+                                                    </ItemBar>
+                                                ) : null,
+                                        )}
+                                    </Box>
+
+                                    <Divider>
+                                        <Typography
+                                            variant="caption"
+                                            fontWeight="bold">
+                                            {numberToCurrency(
+                                                totalNegativeWallet,
+                                            )}
+                                        </Typography>
+                                    </Divider>
+                                </>
+                            )}
+                        </Grid2>
+                    </Grid2>
+                </Grid2>
             </Box>
         </StatCard>
     )
