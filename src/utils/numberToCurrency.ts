2fd996aa1589a700d5692ea0bdc0b966aaeea9a4
Zain Adam <zainadam.id@gmail.com>
2024-02-11 01:53:47 +0800

    chore: add `options` param

--- src/utils/numberToCurrency.ts
+++ src/utils/numberToCurrency.ts
@@ -1,7 +1,11 @@
-export default function numberToCurrency(number: number): string {
+export default function numberToCurrency(
+    number: number,
+    options?: Intl.NumberFormatOptions | undefined,
+): string {
     return number.toLocaleString('id-ID', {
         style: 'currency',
         currency: 'IDR',
         minimumFractionDigits: 0,
+        ...options,
     })
 }
