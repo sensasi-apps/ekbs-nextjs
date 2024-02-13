06d4addb2179b20f6c1036fc9b9f76d652431bec
Zain Adam <zainadam.id@gmail.com>
2024-02-14 04:13:44 +0800

    chore: disabled report page for now

--- src/components/Layouts/MenuData/executiveNavItems.tsx
+++ src/components/Layouts/MenuData/executiveNavItems.tsx
@@ -4,29 +4,29 @@ import type NavItem from './NavItem.type'
 import GroupTitle from './GroupTitle'
 // icons
 import AlignHorizontalLeftIcon from '@mui/icons-material/AlignHorizontalLeft'
-import BackupTableIcon from '@mui/icons-material/BackupTable'
+// import BackupTableIcon from '@mui/icons-material/BackupTable'
 // enums
-import Role from '@/enums/Role'
+import Executive from '@/enums/permissions/Executive'
 
 const executiveNavItems: NavItem[] = [
     {
         children: <GroupTitle>Eksekutif</GroupTitle>,
-        forRole: [Role.EXECUTIVE],
+        forPermission: [Executive.READ_EXECUTIVE_STATISTIC_DATA],
     },
     {
         label: 'Statistik',
         href: '/executive/statistics',
         pathname: '/executive/statistics',
         icon: <AlignHorizontalLeftIcon />,
-        forRole: Role.EXECUTIVE,
+        forPermission: [Executive.READ_EXECUTIVE_STATISTIC_DATA],
     },
-    {
-        label: 'Laporan',
-        href: '/executive/reports',
-        pathname: '/executive/reports',
-        icon: <BackupTableIcon />,
-        forRole: Role.EXECUTIVE,
-    },
+    // {
+    //     label: 'Laporan',
+    //     href: '/executive/reports',
+    //     pathname: '/executive/reports',
+    //     icon: <BackupTableIcon />,
+    //     forRole: Role.EXECUTIVE,
+    // },
 ]
 
 export default executiveNavItems
---
+++ src/enums/permissions/Executive.ts
@@ -0,0 +1,5 @@
+enum Executive {
+    READ_EXECUTIVE_STATISTIC_DATA = 'read executive statistic data',
+}
+
+export default Executive
