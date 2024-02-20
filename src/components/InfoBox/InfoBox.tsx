cf2e5716faea459d4af9b6e19c814f2da9e5b761
Zain Adam <zainadam.id@gmail.com>
2024-02-20 15:28:53 +0800

    chore: init new components

---
+++ src/components/InfoBox/InfoBox.tsx
@@ -0,0 +1,49 @@
+import type { ReactNode } from 'react'
+import Box, { BoxProps } from '@mui/material/Box'
+import Skeleton from '@mui/material/Skeleton'
+import Typography from '@mui/material/Typography'
+
+export default function InfoBox({
+    data,
+    loading,
+    ...props
+}: BoxProps & {
+    data: {
+        label: string
+        value: ReactNode
+    }[]
+    loading?: boolean
+}) {
+    return (
+        <Box component="table" {...props}>
+            <tbody>
+                {data.map(({ label, value }, index) => (
+                    <tr key={index}>
+                        <Typography variant="caption" component="td">
+                            {label}
+                        </Typography>
+
+                        <td
+                            style={{
+                                paddingLeft: '1rem',
+                            }}>
+                            :
+                        </td>
+
+                        <Typography fontWeight="bold" component="td">
+                            {loading ? (
+                                <Skeleton width={randomWidth()} />
+                            ) : (
+                                value
+                            )}
+                        </Typography>
+                    </tr>
+                ))}
+            </tbody>
+        </Box>
+    )
+}
+
+function randomWidth(max = 8, min = 3) {
+    return Math.random() * (max - min) + min + 'em'
+}
