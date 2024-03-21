ddcacc013e11db42427f21a9f846d96616f94294
Zain Adam <zainadam.id@gmail.com>
2024-03-22 04:39:23 +0800

    chore: new components

---
+++ src/components/IconButton/IconButton.tsx
@@ -0,0 +1,23 @@
+import type { SvgIconComponent } from '@mui/icons-material'
+import MuiIconButton, { IconButtonProps } from '@mui/material/IconButton'
+import Tooltip, { TooltipProps } from '../Tooltip'
+
+export default function IconButton({
+    title,
+    icon: Icon,
+    ...props
+}: IconButtonProps & {
+    title: TooltipProps['title']
+    icon: SvgIconComponent
+    href?: string | URL
+    download?: boolean
+    children?: never
+}) {
+    return (
+        <Tooltip title={title}>
+            <MuiIconButton size="small" {...props}>
+                <Icon />
+            </MuiIconButton>
+        </Tooltip>
+    )
+}
---
+++ src/components/Tooltip/Tooltip.tsx
@@ -0,0 +1,19 @@
+import MuiTooltip, {
+    TooltipProps as MuiTooltipProps,
+} from '@mui/material/Tooltip'
+
+export type TooltipProps = MuiTooltipProps
+
+/**
+ * MuiTooltip with default prop
+ *
+ * @param placement top
+ * @param arrow true
+ */
+export default function Tooltip({ children, ...restProps }: TooltipProps) {
+    return (
+        <MuiTooltip placement="top" arrow {...restProps}>
+            <span>{children}</span>
+        </MuiTooltip>
+    )
+}
