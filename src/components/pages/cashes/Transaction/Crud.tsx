d56eb9373cd736f927e3bd648918dd0feeffb724
Zain Adam <zainadam.id@gmail.com>
2024-07-08 13:01:42 +0800

    refactor: move columns def to its only page

--- src/components/pages/cashes/Transaction/Crud.tsx
+++ src/components/pages/cashes/Transaction/Crud.tsx
@@ -1,26 +1,30 @@
 // types
 import type TransactionType from '@/dataTypes/Transaction'
-import type { MUISortOptions } from 'mui-datatables'
+import type { MUIDataTableColumn, MUISortOptions } from 'mui-datatables'
 import type { FormikConfig } from 'formik'
 import type { OnRowClickType } from '@/components/Datatable'
 // vendors
 import { useCallback, useState } from 'react'
 import { Formik } from 'formik'
 import axios from '@/lib/axios'
+// materials
+import green from '@mui/material/colors/green'
 // icons
 import PaymentsIcon from '@mui/icons-material/Payments'
 // components
-import Datatable, { getRowData, mutate } from '@/components/Datatable'
+import Datatable, { getNoWrapCellProps, mutate } from '@/components/Datatable'
 import DialogWithTitle from '@/components/DialogWithTitle'
 import Fab from '@/components/Fab'
 // local components
-import DATATABLE_COLUMNS from './DATATABLE_COLUMNS'
 import TransactionForm, { INITIAL_VALUES, TransactionInitialType } from './Form'
 import { mutate as mutateCashlist } from '../Cash/List'
 // utils
 import errorCatcher from '@/utils/errorCatcher'
+import numberToCurrency from '@/utils/numberToCurrency'
+import toDmy from '@/utils/toDmy'
 
 const DEFAULT_SORT_ORDER: MUISortOptions = { name: 'uuid', direction: 'desc' }
+let getRowData: (dataIndex: number) => TransactionType | undefined
 
 export default function TransactionCrud() {
     const [values, setValues] = useState<
@@ -38,7 +42,7 @@ export default function TransactionCrud() {
 
     const handleRowClick: OnRowClickType = useCallback((_, rowMeta, event) => {
         if (event.detail === 2) {
-            const data = getRowData<TransactionType>(rowMeta.dataIndex)
+            const data = getRowData(rowMeta.dataIndex)
 
             if (data) {
                 setValues(data)
@@ -73,6 +77,7 @@ export default function TransactionCrud() {
                 onRowClick={handleRowClick}
                 columns={DATATABLE_COLUMNS}
                 defaultSortOrder={DEFAULT_SORT_ORDER}
+                getRowDataCallback={fn => (getRowData = fn)}
             />
 
             <DialogWithTitle title={dialogTitle} open={isDialogOpen}>
@@ -93,3 +98,65 @@ export default function TransactionCrud() {
         </>
     )
 }
+
+const DATATABLE_COLUMNS: MUIDataTableColumn[] = [
+    {
+        name: 'uuid',
+        label: 'UUID',
+        options: {
+            display: false,
+        },
+    },
+    {
+        name: 'short_uuid',
+        label: 'Kode',
+        options: {
+            searchable: false,
+            sort: false,
+        },
+    },
+    {
+        name: 'at',
+        label: 'Tanggal',
+        options: {
+            setCellProps: getNoWrapCellProps,
+            customBodyRender: toDmy,
+        },
+    },
+    {
+        name: 'cash.code',
+        label: 'Kode Kas',
+        options: {
+            customBodyRenderLite: dataIndex =>
+                getRowData(dataIndex)?.cash?.code,
+        },
+    },
+    {
+        name: 'amount',
+        label: 'Nilai',
+        options: {
+            customBodyRender: (value: number) => (
+                <span
+                    style={{
+                        whiteSpace: 'nowrap',
+                        color: value <= 0 ? 'inherit' : green[500],
+                    }}>
+                    {numberToCurrency(value)}
+                </span>
+            ),
+        },
+    },
+    {
+        name: 'desc',
+        label: 'Perihal',
+    },
+
+    {
+        name: 'userActivityLogs.user.name',
+        label: 'Oleh',
+        options: {
+            customBodyRenderLite: dataIndex =>
+                getRowData(dataIndex)?.user_activity_logs?.[0]?.user.name,
+        },
+    },
+]
