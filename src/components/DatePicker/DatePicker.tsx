d65aa3536076a6e95b115bcb94b8064ad363333d
Zain Adam <zainadam.id@gmail.com>
2024-07-29 21:30:25 +0800

    chore: expose component's props

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -1,13 +1,14 @@
 // types
-import type { DatePickerProps } from '@mui/x-date-pickers/DatePicker'
+import type { DatePickerProps as MuiDatePickerProps } from '@mui/x-date-pickers/DatePicker'
 import type { Dayjs } from 'dayjs'
-
 // vendors
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
 import { DatePicker as MuiDatePicker } from '@mui/x-date-pickers/DatePicker'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 // components
 import TextField from '@/components/TextField'
+
+type DatePickerProps = MuiDatePickerProps<Dayjs>
 
 /**
  * A date picker component that uses Day.js as the date library.
@@ -19,10 +20,12 @@ export default function DatePicker({
     slots = { textField: TextField },
     format = 'DD-MM-YYYY',
     ...props
-}: DatePickerProps<Dayjs>) {
+}: DatePickerProps) {
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="id">
             <MuiDatePicker slots={slots} format={format} {...props} />
         </LocalizationProvider>
     )
 }
+
+export type { DatePickerProps }
