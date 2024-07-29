92bd5b0296216640fc96cefefd4ba5b6ee7fd3ed
Zain Adam <zainadam.id@gmail.com>
2024-07-29 21:32:56 +0800

    chore: add new db and column types for the feature

---
+++ src/dataTypes/Debt.ts
@@ -0,0 +1,27 @@
+import type { UUID } from 'crypto'
+import type { Ymd } from '@/types/DateString'
+import type BusinessUnit from './BusinessUnit'
+import type InterestUnit from './enums/DbColumns/Debts/InterestUnit'
+import type TermUnit from './enums/DbColumns/Debts/TermUnit'
+import DebtDetail from './DebtDetail'
+
+type Debt = {
+    uuid: UUID
+    business_unit_id: number | null
+    at: Ymd
+    term: number
+    term_unit: TermUnit
+    interest: number
+    interest_unit: InterestUnit
+    base_rp: number
+    note: null | string
+
+    // relations
+    business_unit: BusinessUnit
+    details: DebtDetail[]
+
+    // accessors
+    hasDetails?: boolean
+}
+
+export default Debt
---
+++ src/dataTypes/enums/DbColumns/Debts/TermUnit.ts
@@ -0,0 +1,8 @@
+enum TermUnit {
+    DAYS = 'days',
+    WEEKS = 'weeks',
+    MONTHS = 'months',
+    YEARS = 'years',
+}
+
+export default TermUnit
