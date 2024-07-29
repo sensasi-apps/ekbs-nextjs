ba840999b84ad19aad9c0f46df3353245b8c047b
Zain Adam <zainadam.id@gmail.com>
2024-07-29 21:35:51 +0800

    chore: add new permissions for the feature

--- src/enums/permissions/Finance.ts
+++ src/enums/permissions/Finance.ts
@@ -1,5 +1,10 @@
 enum Finance {
     PAYROLL_EMPLOYEE = 'payroll employee',
+
+    CREATE_DEBT = 'create debt',
+    READ_DEBT = 'read debt',
+    UPDATE_DEBT = 'update debt',
+    SETTLE_DEBT = 'settle debt',
 }
 
 export default Finance
