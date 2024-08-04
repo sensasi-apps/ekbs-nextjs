2c8c5d8a7842b414e0965f6780cb9ddf744cd1d7
Zain Adam <zainadam.id@gmail.com>
2024-08-04 21:43:14 +0800

    chore: add mart product CRUD

---
+++ src/components/pages/marts/products/ApiUrl.ts
@@ -0,0 +1,8 @@
+enum ApiUrl {
+    GET_DATATABLE_DATA = '/marts/products/datatable',
+
+    CREATE_PRODUCT = '/marts/products',
+    UPDATE_OR_DELETE_PRODUCT = '/marts/products/$',
+}
+
+export default ApiUrl
