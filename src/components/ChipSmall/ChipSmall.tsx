fbd8b5a4d0c51443f3d94ac10c1e04708a43fae2
Zain Adam <zainadam.id@gmail.com>
2024-08-12 19:39:37 +0800

    chore: Set `alignItems` to `end` for feels like item in vertically middle

--- src/components/ChipSmall/ChipSmall.tsx
+++ src/components/ChipSmall/ChipSmall.tsx
@@ -3,6 +3,8 @@ import { Chip, type ChipProps } from '@mui/material'
 /**
  * Ordinary MuiChip component with small size by default.
  */
-export function ChipSmall(props: ChipProps) {
-    return <Chip size="small" {...props} />
+export function ChipSmall({ sx, ...restProps }: ChipProps) {
+    return (
+        <Chip size="small" sx={{ alignItems: 'end', ...sx }} {...restProps} />
+    )
 }
