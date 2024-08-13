06213862626229aac99bc8821e0d12893e4e7eda
Zain Adam <zainadam.id@gmail.com>
2024-08-14 01:10:34 +0800

    Revert "chore: Set `alignItems` to `end` for feels like item in vertically middle"
    This reverts commit fbd8b5a4d0c51443f3d94ac10c1e04708a43fae2.

--- src/components/ChipSmall/ChipSmall.tsx
+++ src/components/ChipSmall/ChipSmall.tsx
@@ -3,8 +3,6 @@ import { Chip, type ChipProps } from '@mui/material'
 /**
  * Ordinary MuiChip component with small size by default.
  */
-export function ChipSmall({ sx, ...restProps }: ChipProps) {
-    return (
-        <Chip size="small" sx={{ alignItems: 'end', ...sx }} {...restProps} />
-    )
+export function ChipSmall(props: ChipProps) {
+    return <Chip size="small" {...props} />
 }
