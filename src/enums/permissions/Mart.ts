6c7d3bb2165569b1b52860c1e3022cdec0e1b9a1
Zain Adam <zainadam.id@gmail.com>
2024-08-14 01:13:01 +0800

    chore: Add mart permissions enum

---
+++ src/enums/permissions/Mart.ts
@@ -0,0 +1,18 @@
+enum Mart {
+    READ_PRODUCT = 'read mart product',
+    CREATE_PRODUCT = 'create mart product',
+    UPDATE_PRODUCT = 'update mart product',
+    DELETE_PRODUCT = 'delete mart product',
+
+    READ_PURCHASE = 'read mmart product purchase',
+    CREATE_PURCHASE = 'create mart product purchase',
+    UPDATE_PURCHASE = 'update mart product purchase',
+
+    READ_SALE = 'read mart product sale',
+    CREATE_SALE = 'create mart product sale',
+    UPDATE_SALE = 'update mart product sale',
+
+    READ_CASH = 'read mart cash',
+}
+
+export default Mart
