c5b90d33a0c56701ca99b9a495c03986e211f6f2
Adam Akbar <zainadam.id@gmail.com>
2024-08-17 03:25:10 +0800

    Update authentication features (#328)
    * chore: Disable sw dev log for clean console
    
    * chore: Remove error 401 handler
    
    * chore: Add new `AuthInfo` type
    
    * chore: Remove remembe me checkbox
    
    * chore: Disable google auth temporary
    
    * chore: Define new props for AuthInfo
    
    * chore: Use `user` value instead `isAuthenticated`
    
    * chore: Add sha3 dep
    
    * chore: Add offline auth feat

--- src/components/pages/login/OtherButtons.tsx
+++ src/components/pages/login/OtherButtons.tsx
@@ -1,20 +1,21 @@
 // materials
 import Button from '@mui/material/Button'
 // icons
-import GoogleIcon from '@mui/icons-material/Google'
+// import GoogleIcon from '@mui/icons-material/Google'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
 
 export function OtherButtons() {
     return (
         <>
-            <Button
+            {/* disable google login for now due to offline auth */}
+            {/* <Button
                 href="/api/oauth/google"
                 fullWidth
                 color="inherit"
                 variant="contained"
                 startIcon={<GoogleIcon />}>
                 Login dengan Google
-            </Button>
+            </Button> */}
 
             <Button
                 href="/"
--- src/providers/Auth.tsx
+++ src/providers/Auth.tsx
@@ -1,56 +1,53 @@
 // types
-import type { AxiosError } from 'axios'
-import type UserType from '@/dataTypes/User'
+import { AxiosError } from 'axios'
+import type { AuthInfo } from '@/dataTypes/User'
 // vendors
-import { createContext, useContext, useState, ReactNode } from 'react'
+import { createContext, useContext, ReactNode } from 'react'
+import { sha3_256 } from 'js-sha3'
 import useSWR from 'swr'
 // enums
 import Role from '@/enums/Role'
 import axios from '@/lib/axios'
 
 type AuthContextType = {
-    isAuthenticated: boolean
-    user: UserType | null | undefined
+    user: AuthInfo | null | undefined
     onAgreeTncp: () => void
     userHasPermission: (
         permissionName: string | string[],
-        userParam?: UserType,
+        userParam?: AuthInfo,
     ) => boolean | undefined
     userHasRole: (
         roleName: string | string[],
-        userParam?: UserType,
+        userParam?: AuthInfo,
     ) => boolean | undefined
     onLogoutSuccess: () => void
-    onLoginSuccess: (user: UserType) => void
-    onError401: () => void
+    login: (email: string, password: string) => Promise<void>
 }
 
 const DEFAULT_CONTEXT_VALUE: AuthContextType = {
-    isAuthenticated: false,
     user: null,
     onAgreeTncp: () => {},
     userHasPermission: () => false,
     userHasRole: () => false,
     onLogoutSuccess: () => {},
-    onLoginSuccess: () => {},
-    onError401: () => {},
+    login: async () => {},
 }
 
 const AuthContext = createContext<AuthContextType>(DEFAULT_CONTEXT_VALUE)
 
 export function AuthProvider({ children }: { children: ReactNode }) {
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
     const { data: user, mutate } = useSWR(
         '/api/user',
         () =>
             axios
-                .get<UserType | null>('/api/user')
+                .get<AuthInfo | null>('/api/user')
                 .then(res => res.data)
                 .catch((error: AxiosError) => {
                     if (error.response?.status === 401) {
-                        setIsAuthenticated(false)
+                        return null
                     }
 
+                    // TODO: throw or return reject?
                     throw error
                 }),
         {
@@ -61,80 +58,97 @@ export function AuthProvider({ children }: { children: ReactNode }) {
         },
     )
 
-    const userHasPermission = (
-        permissionName: string | string[],
-        userParam: UserType = user as UserType,
-    ) => {
-        if (!userParam) return
-
-        if (userParam.role_names?.includes(Role.SUPERMAN)) {
-            return true
-        }
-
-        if (permissionName instanceof Array) {
-            return (
-                permissionName.findIndex(p =>
-                    userParam.permission_names?.includes(p),
-                ) !== -1
-            )
-        }
-
-        return userParam.permission_names?.includes(permissionName)
-    }
-
-    const userHasRole = (
-        roleName: string | string[],
-        userParam: UserType = user as UserType,
-    ) => {
-        if (!userParam) return
-
-        if (userParam.role_names?.includes(Role.SUPERMAN)) {
-            return true
-        }
-
-        if (roleName instanceof Array) {
-            return Boolean(
-                roleName.findIndex(r => userParam.role_names?.includes(r)) !==
-                    -1 ||
-                    roleName.findIndex(r =>
-                        userParam.role_names_id?.includes(r),
-                    ) !== -1,
-            )
-        }
-
-        return Boolean(
-            userParam.role_names?.includes(roleName) ||
-                userParam.role_names_id?.includes(roleName),
-        )
-    }
-
-    const ContextValue: AuthContextType = {
-        isAuthenticated,
-        user,
-        onAgreeTncp: () => {
-            if (user) {
-                mutate({ ...user, is_agreed_tncp: true })
-            }
-        },
-        onLoginSuccess: (userParam: UserType) => {
-            setIsAuthenticated(true)
-            mutate(userParam)
-        },
-        onLogoutSuccess: async () => {
-            const cache = await caches.open('auth-user-cache')
-            const keys = await cache.keys()
-            keys.forEach(async req => await cache.delete(req))
-
-            mutate(null)
-            setIsAuthenticated(false)
-        },
-        userHasPermission,
-        userHasRole,
-        onError401: () => setIsAuthenticated(false),
-    }
-
     return (
-        <AuthContext.Provider value={ContextValue}>
+        <AuthContext.Provider
+            value={{
+                user,
+                onAgreeTncp: () => {
+                    if (user) {
+                        mutate({ ...user, is_agreed_tncp: true })
+                    }
+                },
+                onLogoutSuccess: async () => {
+                    mutate(null)
+                },
+                userHasPermission: (
+                    permissionName: string | string[],
+                    userParam: AuthInfo = user as AuthInfo,
+                ) => {
+                    if (!userParam) return
+
+                    if (userParam.role_names?.includes(Role.SUPERMAN)) {
+                        return true
+                    }
+
+                    if (permissionName instanceof Array) {
+                        return (
+                            permissionName.findIndex(p =>
+                                userParam.permission_names?.includes(p),
+                            ) !== -1
+                        )
+                    }
+
+                    return userParam.permission_names?.includes(permissionName)
+                },
+                userHasRole: (
+                    roleName: string | string[],
+                    userParam: AuthInfo = user as AuthInfo,
+                ) => {
+                    if (!userParam) return
+
+                    if (userParam.role_names?.includes(Role.SUPERMAN)) {
+                        return true
+                    }
+
+                    if (roleName instanceof Array) {
+                        return Boolean(
+                            roleName.findIndex(r =>
+                                userParam.role_names?.includes(r),
+                            ) !== -1 ||
+                                roleName.findIndex(r =>
+                                    userParam.role_names_id?.includes(r),
+                                ) !== -1,
+                        )
+                    }
+
+                    return Boolean(
+                        userParam.role_names?.includes(roleName) ||
+                            userParam.role_names_id?.includes(roleName),
+                    )
+                },
+                login: async (email: string, password: string) => {
+                    const hash = sha3_256(email.split('@')[0] + password)
+
+                    return axios
+                        .post<undefined>('/login', { email, password })
+                        .then(() => {
+                            mutate().then(authInfo => {
+                                localStorage.setItem(
+                                    hash,
+                                    JSON.stringify(authInfo),
+                                )
+                            })
+                        })
+                        .catch((error: AxiosError) => {
+                            const localData = localStorage.getItem(hash)
+
+                            if (
+                                error.code === AxiosError.ERR_NETWORK &&
+                                localData
+                            ) {
+                                const authInfo = JSON.parse(
+                                    localData,
+                                ) as AuthInfo
+
+                                mutate(authInfo).catch(() => authInfo)
+
+                                return
+                            }
+
+                            return Promise.reject(error)
+                        })
+                },
+            }}>
             {children}
         </AuthContext.Provider>
     )
