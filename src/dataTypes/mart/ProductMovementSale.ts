b6b2b9c760cebeeef0bb3e3761f82ee8c9968190
Zain Adam <zainadam.id@gmail.com>
2024-08-22 18:01:13 +0800

    chore: Add `buyer_user` relation

--- src/dataTypes/mart/ProductMovementSale.ts
+++ src/dataTypes/mart/ProductMovementSale.ts
@@ -3,6 +3,9 @@ import type User from '../User'
 type ProductMovementSale = {
     buyer_user_uuid: User['uuid']
     no: number
+
+    // relations
+    buyer_user?: User
 }
 
 export default ProductMovementSale
