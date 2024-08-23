585f7f9753eabfb3ce871a3b23a2d3c26ff2e36b
Zain Adam <zainadam.id@gmail.com>
2024-08-23 19:14:35 +0800

    chore: Add new `DateTimePicker` component

---
+++ src/components/DateTimePicker/DateTimePicker.tsx
@@ -0,0 +1,37 @@
+// types
+import type { Dayjs } from 'dayjs'
+// vendors
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import {
+    DateTimePicker as MuiDateTimePicker,
+    DateTimePickerProps as MuiDateTimePickerProps,
+} from '@mui/x-date-pickers'
+// components
+import TextField from '@/components/TextField'
+
+type DateTimePickerProps = MuiDateTimePickerProps<Dayjs>
+
+/**
+ * A date picker component that uses Day.js as the date library.
+ * @param {DatePickerProps} props - DatePickerProps.
+ * @param slots - default: { textField: TextField }.
+ * @param format - default: `DD-MM-YYYY`.
+ */
+export default function DateTimePicker({
+    slots = { textField: TextField },
+    format = 'DD-MM-YYYY HH:mm',
+    orientation = 'landscape',
+    ...props
+}: DateTimePickerProps) {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="id">
+            <MuiDateTimePicker
+                slots={slots}
+                orientation={orientation}
+                format={format}
+                {...props}
+            />
+        </LocalizationProvider>
+    )
+}
