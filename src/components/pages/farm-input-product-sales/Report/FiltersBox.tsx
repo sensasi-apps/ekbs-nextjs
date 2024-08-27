310d0a9d8e5f32aa7688ae5757257e4c8aa5777a
Zain Adam <zainadam.id@gmail.com>
2024-08-27 23:07:31 +0800

    feat: Add Table components for farm input product sales report
    The code changes include adding three new files: TableBody.tsx, TableHead.tsx, and TableFooter.tsx. These files contain the components for the table body, table head, and table footer of the farm input product sales report table. The components are responsible for rendering the data, headers, and footer of the table respectively. This addition improves the organization and structure of the code for the report table.

---
+++ src/components/pages/farm-input-product-sales/Report/FiltersBox.tsx
@@ -0,0 +1,118 @@
+// vendors
+import { Box } from '@mui/material'
+import { useRouter } from 'next/router'
+import dayjs, { Dayjs } from 'dayjs'
+// components
+import DatePicker from '@/components/DatePicker'
+import IconButton from '@/components/IconButton'
+// icons
+// import BackupTableIcon from '@mui/icons-material/BackupTable'
+import RefreshIcon from '@mui/icons-material/Refresh'
+// import { apiUrl } from '@/pages/farm-input-product-sales/report'
+import { useEffect, useState } from 'react'
+
+const MAX_DATE = dayjs().endOf('month')
+const MIN_DATE = dayjs('2024-01-01')
+
+export default function FiltersBox({
+    disabled,
+    onRefresh,
+}: {
+    disabled: boolean
+    onRefresh: () => void
+}) {
+    const {
+        query: { from_date, till_date },
+        replace,
+    } = useRouter()
+
+    const [fromDate, setFromDate] = useState<Dayjs | null>(
+        from_date ? dayjs(from_date as string) : null,
+    )
+    const [tillDate, setTillDate] = useState<Dayjs | null>(
+        till_date ? dayjs(till_date as string) : null,
+    )
+
+    useEffect(() => {
+        console.log('from_date', from_date)
+
+        if (from_date !== fromDate?.format('YYYY-MM-DD')) {
+            setFromDate(dayjs(from_date as string))
+        }
+
+        if (till_date !== tillDate?.format('YYYY-MM-DD')) {
+            setTillDate(dayjs(till_date as string))
+        }
+    }, [from_date, till_date])
+
+    return (
+        <Box display="flex" gap={2}>
+            <DatePicker
+                disabled={disabled}
+                label="Dari Tanggal"
+                minDate={MIN_DATE}
+                disableHighlightToday
+                maxDate={tillDate ?? MAX_DATE}
+                value={fromDate}
+                onChange={value => setFromDate(value)}
+                slotProps={{
+                    textField: {
+                        margin: 'none',
+                    },
+                }}
+            />
+
+            <DatePicker
+                disabled={disabled}
+                label="Hingga Tanggal"
+                minDate={fromDate ?? MIN_DATE}
+                disableHighlightToday
+                maxDate={MAX_DATE}
+                value={tillDate}
+                onChange={value => setTillDate(value)}
+                slotProps={{
+                    textField: {
+                        margin: 'none',
+                    },
+                }}
+            />
+
+            <IconButton
+                title="Segarkan"
+                disabled={!(fromDate || tillDate || disabled)}
+                icon={RefreshIcon}
+                onClick={() => {
+                    const newQuery = {
+                        from_date: fromDate?.format('YYYY-MM-DD'),
+                        till_date: tillDate?.format('YYYY-MM-DD'),
+                    }
+
+                    const isQueryChanged =
+                        JSON.stringify(newQuery) !==
+                        JSON.stringify({ from_date, till_date })
+
+                    if (isQueryChanged) {
+                        replace({
+                            query: newQuery,
+                        })
+                    } else {
+                        onRefresh()
+                    }
+                }}
+            />
+
+            {/* <IconButton
+                color="success"
+                disabled={!Boolean(from_date || till_date) || disabled}
+                title="Unduh Excel"
+                icon={BackupTableIcon}
+                href={`${
+                    process.env.NEXT_PUBLIC_BACKEND_URL
+                }/${apiUrl}?from_date=${fromDate?.format(
+                    'YYYY-MM-DD',
+                )}&till_date=${tillDate?.format('YYYY-MM-DD')}&excel=true`}
+                download
+            /> */}
+        </Box>
+    )
+}
---
+++ src/components/pages/farm-input-product-sales/Report/Table/TableRow.tsx
@@ -0,0 +1,149 @@
+import ChipSmall from '@/components/ChipSmall'
+import ProductSaleType from '@/dataTypes/ProductSale'
+import globalFormatNumber from '@/utils/formatNumber'
+import toDmy from '@/utils/toDmy'
+import ucWords from '@/utils/ucWords'
+import { TableCell, TableRow as MuiTableRow } from '@mui/material'
+import { memo, ReactNode } from 'react'
+
+const LEFT_BORDER_STYLE = {
+    borderLeft: '1px solid var(--mui-palette-TableCell-border)',
+}
+
+function TableRow({
+    data: {
+        product_movement_details,
+        product_movement,
+        at,
+        buyer_user,
+        payment_method_id,
+        total_base_rp,
+        total_rp,
+    },
+    no,
+}: {
+    data: ProductSaleType
+    no: number
+}) {
+    const totalBaseCostRp = product_movement_details.reduce(
+        (acc, pmd) =>
+            acc +
+            pmd.qty * -1 * pmd.product_warehouse_state.base_cost_rp_per_unit,
+        0,
+    )
+
+    const adjustedTotalRp = total_rp - total_base_rp
+
+    const marginRp = total_rp - totalBaseCostRp
+    const marginPercentage = (marginRp / total_rp) * 100
+
+    return (
+        <MuiTableRow>
+            <TableCell>{no}</TableCell>
+            <TableCell>{toDmy(at)}</TableCell>
+            <TableCell>{ucWords(product_movement.warehouse)}</TableCell>
+            <TableCell>
+                {buyer_user ? `#${buyer_user.id} ${buyer_user.name}` : '-'}
+            </TableCell>
+            <TableCell>{payment_method_id}</TableCell>
+
+            <TableCell>
+                <Ul>
+                    {product_movement_details.map(
+                        ({ product_state: { name } }, i) => (
+                            <li key={i}>{name}</li>
+                        ),
+                    )}
+                </Ul>
+            </TableCell>
+
+            <TableCell align="right">
+                <Ul>
+                    {product_movement_details.map(({ qty }, i) => (
+                        <li key={i}>{formatNumber(Math.abs(qty))}</li>
+                    ))}
+                </Ul>
+            </TableCell>
+
+            <TableCell>
+                <Ul>
+                    {product_movement_details.map(
+                        ({ product_state: { unit } }, i) => (
+                            <li key={i}>{unit}</li>
+                        ),
+                    )}
+                </Ul>
+            </TableCell>
+
+            <TableCell align="right">
+                <Ul>
+                    {product_movement_details.map(
+                        (
+                            {
+                                product_warehouse_state: {
+                                    base_cost_rp_per_unit,
+                                },
+                            },
+                            i,
+                        ) => (
+                            <li key={i}>
+                                {formatNumber(base_cost_rp_per_unit)}
+                            </li>
+                        ),
+                    )}
+                </Ul>
+            </TableCell>
+
+            <TableCell align="right">{formatNumber(totalBaseCostRp)}</TableCell>
+
+            <TableCell sx={LEFT_BORDER_STYLE}>
+                <Ul>
+                    {product_movement_details.map(({ rp_per_unit }, i) => (
+                        <li key={i}>{formatNumber(rp_per_unit)}</li>
+                    ))}
+                </Ul>
+            </TableCell>
+
+            <TableCell align="right">{formatNumber(total_base_rp)}</TableCell>
+
+            <TableCell align="right">
+                {adjustedTotalRp ? formatNumber(adjustedTotalRp) : ''}
+            </TableCell>
+
+            <TableCell align="right">{formatNumber(total_rp)}</TableCell>
+
+            <TableCell align="right">
+                {formatNumber(marginRp)}
+
+                <ChipSmall
+                    sx={{ ml: 2 }}
+                    color={marginPercentage >= 7 ? 'success' : 'warning'}
+                    variant="outlined"
+                    label={formatNumber(marginPercentage) + '%'}
+                />
+            </TableCell>
+        </MuiTableRow>
+    )
+}
+
+export default memo(TableRow)
+
+function Ul({ children }: { children: ReactNode }) {
+    return (
+        <ul
+            style={{
+                margin: 0,
+                paddingLeft: '1em',
+                whiteSpace: 'nowrap',
+            }}>
+            {children}
+        </ul>
+    )
+}
+
+function formatNumber(number: number) {
+    return globalFormatNumber(number, {
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2,
+    })
+}
