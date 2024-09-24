ca91ffc136e1d6397937e728a8a0b8f582b1c332
Zain Adam <zainadam.id@gmail.com>
2024-09-24 16:51:56 +0800

    Add `null` coalescing on transale function Fix #359

--- src/locales/__.ts
+++ src/locales/__.ts
@@ -1,13 +1,19 @@
+// TODO: REMOVE THIS FILE
+
 enum LocaleEnum {
     ID = 'id',
 }
 
 const cache: {
-    [locale: string]: {
-        [scope: string]: {
-            [key: string]: string
-        }
-    }
+    [locale: string]:
+        | {
+              [scope: string]:
+                  | {
+                        [key: string]: string | undefined
+                    }
+                  | undefined
+          }
+        | undefined
 } = {}
 
 const __ = (
@@ -19,10 +25,12 @@ const __ = (
 
     if (!cache[locale][scope])
         import(`./${locale}/${scope}.json`).then(data => {
+            if (!cache[locale]) cache[locale] = {}
+
             cache[locale][scope] = data
         })
 
-    return cache[locale][scope][key] || key
+    return cache[locale]?.[scope]?.[key] || key
 }
 
 export default __
