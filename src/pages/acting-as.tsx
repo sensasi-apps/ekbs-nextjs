10604a1c7178b796ee9c6c1dc94cf8b468f84f8d
Zain Adam <zainadam.id@gmail.com>
2024-10-15 00:59:26 +0800

    implement new auth mechanism for `acting as` feature

--- src/pages/acting-as.tsx
+++ src/pages/acting-as.tsx
@@ -1,11 +1,17 @@
+// types
+import type { AuthInfo } from '@/@types/Data/auth-info'
 // vendors
 import { useState } from 'react'
 // components
 import AuthLayout from '@/components/Layouts/AuthLayout'
 import UserAutocomplete from '@/components/UserAutocomplete'
 import axios from '@/lib/axios'
+import { getDeviceId } from '@/functions/getDeviceId'
+import { useRouter } from 'next/navigation'
+import { setCurrentAuthInfo } from '@/providers/Auth/functions/setCurrentAuthInfo'
 
 export default function ActingAs() {
+    const router = useRouter()
     const [loading, setLoading] = useState(false)
 
     return (
@@ -17,9 +23,15 @@ export default function ActingAs() {
                     if (!user) return
 
                     setLoading(true)
-                    return axios
-                        .post(`/acting-as/${user.uuid}`)
-                        .then(() => (window.location.href = '/dashboard'))
+
+                    axios
+                        .post<AuthInfo>(`/acting-as/${user.uuid}`, {
+                            device_id: getDeviceId(),
+                        })
+                        .then(res => {
+                            setCurrentAuthInfo(res.data)
+                            router.push('/')
+                        })
                         .catch(() => setLoading(false))
                 }}
             />
