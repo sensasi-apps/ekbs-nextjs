f094786d3a2dffc5a640c4ee65a7cb0a06ccee45
Zain Adam <zainadam.id@gmail.com>
2024-10-16 17:43:35 +0800

    Refactor enum value in Mart.ts to fix typo in permission name

--- src/enums/permissions/Mart.ts
+++ src/enums/permissions/Mart.ts
@@ -4,7 +4,7 @@ enum Mart {
     UPDATE_PRODUCT = 'update mart product',
     DELETE_PRODUCT = 'delete mart product',
 
-    READ_PURCHASE = 'read mmart product purchase',
+    READ_PURCHASE = 'read mart product purchase',
     CREATE_PURCHASE = 'create mart product purchase',
     UPDATE_PURCHASE = 'update mart product purchase',
 
