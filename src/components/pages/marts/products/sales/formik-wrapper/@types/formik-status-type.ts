a3564d08d4c7c6300840a84083fc730d5b79a3cc
Zain Adam <zainadam.id@gmail.com>
2024-10-30 21:09:47 +0800

    Enhance FormikStatusType and update validation schema: Add optional 'no' field and refine costs validation

--- src/components/pages/marts/products/sales/formik-wrapper/@types/formik-status-type.ts
+++ src/components/pages/marts/products/sales/formik-wrapper/@types/formik-status-type.ts
@@ -3,5 +3,7 @@ import { FormValuesType } from './form-values-type'
 export interface FormikStatusType {
     isDisabled: boolean
     isFormOpen: boolean
-    submittedData?: Required<FormValuesType>
+    submittedData?: Required<FormValuesType> & {
+        no?: FormValuesType['no']
+    }
 }
