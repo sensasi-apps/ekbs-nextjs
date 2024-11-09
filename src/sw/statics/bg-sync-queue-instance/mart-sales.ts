ed93c44774b8f5b070f7d970d889d963f712ad11
Zain Adam <zainadam.id@gmail.com>
2024-11-10 04:58:50 +0800

    Add new `handleOnSync` function to handle bg sync

--- src/sw/statics/bg-sync-queue-instance/mart-sales.ts
+++ src/sw/statics/bg-sync-queue-instance/mart-sales.ts
@@ -1,5 +1,9 @@
+import { handleOnSync } from '@/sw/functions/handle-on-sync'
 import { BackgroundSyncQueue } from 'serwist'
 
 export const martSales = new BackgroundSyncQueue('martSales', {
     maxRetentionTime: 60 * 24 * 60, // 2 months
+    onSync: async ({ queue }) => {
+        await handleOnSync(queue)
+    },
 })
