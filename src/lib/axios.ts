a52e3b3f245e47a877b8349c215c804b24d12f84
Zain Adam <zainadam.id@gmail.com>
2024-11-11 04:57:13 +0800

    Improve error handling for CSRF cookie request in axios configuration

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,7 +6,7 @@ import { handleServerError } from './axios/functions/handle-server-error'
 import { getCurrentAuthToken } from './axios/getCurrentAuthToken'
 
 /**
- * TODO: REDUCE `csrf-cookie` REQUEST IF POSSIBLE.
+ * @todo REDUCE `csrf-cookie` REQUEST IF POSSIBLE.
  */
 const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -14,7 +14,20 @@ const axios = Axios.create({
     withCredentials: true,
 })
 
-axios.get('/sanctum/csrf-cookie')
+axios.get('/sanctum/csrf-cookie').catch((error: AxiosError) => {
+    const { response, code, message } = error
+
+    if (response) {
+        handleServerError(response)
+    } else if (code !== AxiosError.ERR_NETWORK) {
+        enqueueSnackbar(message ?? 'Terjadi kesalahan pada server.', {
+            variant: 'error',
+            persist: true,
+        })
+
+        Promise.reject(error)
+    }
+})
 
 axios.interceptors.request.use(config => {
     const token = getCurrentAuthToken()
@@ -32,9 +45,11 @@ axios.interceptors.response.use(undefined, (error: AxiosError) => {
     const { response, code } = error
 
     if (response) {
-        handleServerError(response)
-    } else if (code === AxiosError.ERR_NETWORK) {
-        enqueueSnackbar(
+        return handleServerError(response)
+    }
+
+    if (code === AxiosError.ERR_NETWORK) {
+        return enqueueSnackbar(
             'Permintaan gagal dikirimkan, mohon periksa kembali koneksi internet anda.',
             {
                 variant: 'error',
