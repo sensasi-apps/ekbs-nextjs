a650feb5e1002edf40c2b3513762ca2858ad5d36
Adam Akbar <zainadam.id@gmail.com>
2024-11-17 11:28:30 +0800

    Fix API URL for product opname creation by removing trailing slash (#495)

--- src/enums/ApiUrl/Mart/Product/Opname.ts
+++ src/enums/ApiUrl/Mart/Product/Opname.ts
@@ -5,7 +5,7 @@ enum OpnameApiUrl {
     PRODUCTS_DATA = ApiUrl.PRODUCTS,
     DATATABLE = 'marts/products/opnames/datatable',
 
-    CREATE = 'marts/products/opnames/',
+    CREATE = 'marts/products/opnames',
     ADD_PRODUCTS = 'marts/products/opnames/$/add-products',
     GET_DETAIL = 'marts/products/opnames/$',
     REMOVE_PRODUCT = 'marts/products/opnames/$pmdId/remove-product',
