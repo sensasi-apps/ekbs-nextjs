b90940475ed656a5db8397eefb6defaba6772137
Zain🍕 <zainadam.id@gmail.com>
2024-12-17 06:01:59 +0800

    Add fake loading to prevent flickering on homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
 // vendors
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 // component
 import { Landing } from '@/components/pages/index/landing'
 // providers
 import useAuth from '@/providers/Auth'
+import LogoLoadingBox from '@/components/LogoLoadingBox'
+
+let timeout: NodeJS.Timeout
 
 export default function Index() {
     const { replace } = useRouter()
     const { user } = useAuth()
+    const [showContent, setShowContent] = useState(false)
 
     useEffect(() => {
-        if (user) {
-            replace('/dashboard')
-        }
+        clearTimeout(timeout)
+
+        timeout = setTimeout(() => {
+            if (user) {
+                replace('/dashboard')
+            } else {
+                setShowContent(true)
+            }
+        }, 3000)
     }, [user, replace])
 
-    return <Landing />
+    return showContent ? <Landing /> : <LogoLoadingBox />
 }
