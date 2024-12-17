fa1f4975293084d9dc646624b474b8c671d17bb3
Zain🍕 <zainadam.id@gmail.com>
2024-12-18 03:25:58 +0800

    Remove environment preview's alert and Add greeting from me

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,16 @@
 import type { AppProps } from 'next/app'
-import { Typography } from '@mui/material'
 import { AppProviders } from '@/providers/app-providers'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        // eslint-disable-next-line
+        console.log("You've got a 🍕 from @sensasi-delight")
+    }, [])
+
     return (
         <AppProviders>
             <Component {...pageProps} />
-
-            {process.env.VERCEL_ENV === 'preview' && (
-                <Typography
-                    position="fixed"
-                    bottom="0"
-                    variant="overline"
-                    fontSize="1rem"
-                    align="center"
-                    width="100%"
-                    bgcolor="warning.dark"
-                    color="warning.contrastText"
-                    fontWeight="bold"
-                    zIndex="tooltip"
-                    lineHeight="unset"
-                    p={2}
-                    style={{
-                        pointerEvents: 'none',
-                        opacity: 0.5,
-                    }}>
-                    <div>Halaman ini hanya untuk tujuan demonstrasi</div>
-                </Typography>
-            )}
         </AppProviders>
     )
 }
