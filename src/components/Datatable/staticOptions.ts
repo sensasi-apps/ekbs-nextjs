7abe3501e14dfa0f9050d05a0a2f4a9740fe072c
Zain🍕 <zainadam.id@gmail.com>
2024-12-30 23:39:32 +0800

    Upgrade `<Datatable />` to use `mui-datatable-delight` and update text labels for better localization

--- src/components/Datatable/staticOptions.ts
+++ src/components/Datatable/staticOptions.ts
@@ -1,13 +1,32 @@
-import { type MUIDataTableOptions } from 'mui-datatables'
+import type { DataTableOptions } from 'mui-datatable-delight'
 
-const staticOptions: MUIDataTableOptions = {
+const STATIC_TEXT_LABELS = {
+    pagination: {
+        next: 'selanjutnya',
+        previous: 'sebelumnya',
+        rowsPerPage: 'data/halaman:',
+        jumpToPage: 'halaman:',
+    },
+    toolbar: {
+        search: 'Cari',
+        downloadCsv: 'Unduh',
+        print: 'Cetak',
+        viewColumns: 'Tampilkan kolom',
+    },
+    body: {
+        noMatch: 'Tidak ada data yang tersedia',
+        toolTip: 'Urutkan',
+    },
+}
+
+const staticOptions: DataTableOptions = {
     filter: false,
     serverSide: true,
     responsive: 'standard',
     selectableRows: 'none',
     print: false,
     jumpToPage: true,
-    rowsPerPageOptions: [15, 30, 50, 100],
+    textLabels: STATIC_TEXT_LABELS,
 }
 
 export default staticOptions
