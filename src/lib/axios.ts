12e530bdce962613997792aaa2cd5413cc56a7b1
Zain🍕 <zainadam.id@gmail.com>
2025-01-06 06:25:50 +0800

    Change Axios request to stateless

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,33 +5,30 @@ import axios, { AxiosError } from 'axios'
 import { handleServerError } from './axios/functions/handle-server-error'
 import { getCurrentAuthToken } from './axios/getCurrentAuthToken'
 
-/**
- * @todo REDUCE `csrf-cookie` REQUEST IF POSSIBLE.
- */
 const myAxios = axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    withXSRFToken: true,
+    // withXSRFToken: true,
     withCredentials: true,
 })
 
-if (typeof window !== 'undefined') {
-    myAxios.get('/sanctum/csrf-cookie').catch((error: AxiosError) => {
-        const { response, code, message } = error
+// if (typeof window !== 'undefined') {
+//     myAxios.get('/sanctum/csrf-cookie').catch((error: AxiosError) => {
+//         const { response, code, message } = error
 
-        if (response) {
-            handleServerError(response)
-        } else if (code !== AxiosError.ERR_NETWORK) {
-            enqueueSnackbar(message ?? 'Terjadi kesalahan.', {
-                variant: 'error',
-                persist: true,
-            })
-        }
+//         if (response) {
+//             handleServerError(response)
+//         } else if (code !== AxiosError.ERR_NETWORK) {
+//             enqueueSnackbar(message ?? 'Terjadi kesalahan.', {
+//                 variant: 'error',
+//                 persist: true,
+//             })
+//         }
 
-        if (response || code !== AxiosError.ERR_NETWORK) {
-            throw error
-        }
-    })
-}
+//         if (response || code !== AxiosError.ERR_NETWORK) {
+//             throw error
+//         }
+//     })
+// }
 
 myAxios.interceptors.request.use(config => {
     const token = getCurrentAuthToken()
