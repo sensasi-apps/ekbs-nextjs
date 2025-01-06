1fccd89b60907c5fede49ea70539a5e963911594
Zain🍕 <zainadam.id@gmail.com>
2025-01-06 10:03:29 +0800

    Update service worker configuration to exclude problematic files and enhance caching strategy

--- src/sw.ts
+++ src/sw.ts
@@ -20,12 +20,15 @@ declare global {
 declare const self: ServiceWorkerGlobalScope
 
 const serwist = new Serwist({
-    precacheEntries: self.__SW_MANIFEST,
-    skipWaiting: true,
     clientsClaim: true,
-    navigationPreload: true,
     disableDevLogs: true,
+    navigationPreload: true,
+    precacheEntries: self.__SW_MANIFEST,
+    precacheOptions: {
+        cleanupOutdatedCaches: true,
+    },
     runtimeCaching: defaultCache,
+    skipWaiting: true,
 })
 
 self.addEventListener('fetch', handleFetch)
