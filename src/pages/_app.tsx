646a1aeba1166febd6d48b89d2888febc686d98a
Zain🍕 <zainadam.id@gmail.com>
2025-01-14 15:37:41 +0800

    Revert "Integrate `MUI` with `Nextjs` pages router"
    This reverts commit c5a968fe211751236f27105188bdb5cbe17d96ed.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,16 +2,14 @@ import type { AppProps } from 'next/app'
 import { AppProviders } from '@/providers/app-providers'
 import { useEffect } from 'react'
 
-export default function App(props: AppProps) {
-    const { Component, pageProps } = props
-
+export default function App({ Component, pageProps }: AppProps) {
     useEffect(() => {
         // eslint-disable-next-line
         console.log("You've got a 🍕 from @sensasi-delight")
     }, [])
 
     return (
-        <AppProviders {...props}>
+        <AppProviders>
             <Component {...pageProps} />
         </AppProviders>
     )
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,11 @@
-import {
-    type DocumentHeadTagsProps,
-    DocumentHeadTags,
-    documentGetInitialProps,
-} from '@mui/material-nextjs/v15-pagesRouter'
-import {
-    type DocumentProps,
-    type DocumentContext,
-    Html,
-    Head,
-    Main,
-    NextScript,
-} from 'next/document'
+// import { getInitColorSchemeScript } from '@mui/material'
 import InitColorSchemeScript from '@mui/material/InitColorSchemeScript'
+import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
+export default function Document() {
     return (
         <Html lang="id" data-mui-color-scheme="light">
             <Head>
-                <DocumentHeadTags {...props} />
                 <meta charSet="utf8" />
 
                 <meta
@@ -99,9 +87,3 @@ const FacebookMetaTags = () => (
         />
     </>
 )
-
-Document.getInitialProps = async (ctx: DocumentContext) => {
-    const finalProps = await documentGetInitialProps(ctx)
-
-    return finalProps
-}
