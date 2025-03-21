09446c690383e8369b2a79f854d7360305c245ef
Zain🍕 <zainadam.id@gmail.com>
2025-03-21 13:34:38 +0800

    Refactor `DatatableRequest`

--- src/components/Datatable/utils/formatToDatatableParams.ts
+++ src/components/Datatable/utils/formatToDatatableParams.ts
@@ -1,8 +1,6 @@
-import type { DataTableRequest } from '@/@types/request/datatable'
 import type { DataTableState } from 'mui-datatable-delight'
 
 /**
- *
  * @param tableState
  * @returns datatable params
  *
@@ -42,4 +40,31 @@ function formatColumns<T>(
         searchable: searchable ?? true,
         orderable: sort ?? true,
     }))
+}
+
+/**
+ * @see https://datatables.net/manual/server-side#Sent-parameters
+ */
+interface DataTableRequest {
+    draw: number
+    start: number
+    length: number
+    search: {
+        value: string | null
+        regex: boolean
+    }
+    order: {
+        column: number
+        dir: string
+    }[]
+    columns: {
+        data: string
+        name: string
+        searchable: boolean
+        orderable: boolean
+        search?: {
+            value: string
+            regex: boolean
+        }
+    }[]
 }
