1287152fe228c3e9ca7d46f659e3d8249c011c19
sensasi-delight <zainadam.id@gmail.com>
2025-04-28 18:04:32 +0800

    Add repair shop roles for user; update permission name (#657)

--- src/features/repair-shop--spare-part/enums/permission.ts
+++ src/features/repair-shop--spare-part/enums/permission.ts
@@ -1,8 +1,11 @@
+/**
+ * Make sure this is sync with {@link https://github.com/sensasi-apps/ekbs-laravel/blob/main/Modules/RepairShop/app/Enums/Permission.php|permission enum on backend}
+ */
 enum Permission {
-    READ = 'read spare part',
-    CREATE = 'create spare part',
-    UPDATE = 'update spare part',
-    DELETE = 'delete spare part',
+    READ = '[repair shop] read spare part',
+    CREATE = '[repair shop] create spare part',
+    UPDATE = '[repair shop] update spare part',
+    DELETE = '[repair shop] delete spare part',
 }
 
 export default Permission
