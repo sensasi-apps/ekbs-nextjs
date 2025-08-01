adefe0766a90b05e3efabf5dff6b3ddf9d6e14c1
Zain🍕 <zainadam.id@gmail.com>
2025-08-01 14:16:22 +0800

    reduce font size

--- src/components/ChipSmall/ChipSmall.tsx
+++ src/components/ChipSmall/ChipSmall.tsx
@@ -4,5 +4,13 @@ import Chip, { type ChipProps } from '@mui/material/Chip'
  * Ordinary MuiChip component with small size by default.
  */
 export function ChipSmall(props: ChipProps) {
-    return <Chip size="small" {...props} />
+    return (
+        <Chip
+            size="small"
+            sx={{
+                fontSize: '0.75rem',
+            }}
+            {...props}
+        />
+    )
 }
