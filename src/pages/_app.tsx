sensasi-apps/ekbs-nextjs

commit 7cfe4f327ecb966992af86cee56e22755237fefc

improve SSR for `mui` (#700)
- implement server-cached CSS.
- setup layout accommodation for pages.
- replace `@fontsource/roboto` with `next/font/google`.
- change attribute name for storing color scheme value from `data-mui-color-scheme` to `class`.

src/pages/_app.tsx
@@ -1,16 +1,56 @@
 import type { AppProps } from 'next/app'
-import { AppProviders } from '@/providers/app-providers'
-import { useEffect } from 'react'
+import type { NextPage } from 'next'
+import { AppCacheProvider } from '@mui/material-nextjs/v15-pagesRouter'
+import { useEffect, type ReactElement, type ReactNode } from 'react'
+import { Roboto } from 'next/font/google'
+import Head from 'next/head'
 
-export default function App({ Component, pageProps }: AppProps) {
+import { AppProviders } from '@/providers/app-providers'
+
+const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+    variable: '--font-roboto',
+})
+
+/**
+ * references:
+ * - [AppProviders](https://github.com/mui/material-ui/blob/25984d5f9b4161f8feeeeb98a4e89903a163f5e0/docs/data/material/integrations/nextjs/nextjs.md?plain=1#L224-L229)
+ * - [getLayout](https://nextjs.org/docs/pages/building-your-application/routing/pages-and-layouts)
+ */
+export default function App(props: AppPropsWithLayout) {
+    const { Component, pageProps } = props
+
     useEffect(() => {
         // eslint-disable-next-line
         console.log("You've got a 🍕 from @sensasi-delight")
     }, [])
 
-    return (
-        <AppProviders>
-            <Component {...pageProps} />
-        </AppProviders>
+    // Use the layout defined at the page level, if available
+    const getLayout = Component.getLayout ?? (page => page)
+
+    const componentClassName = [pageProps.className, roboto.className]
+        .filter(Boolean)
+        .join(' ')
+
+    return getLayout(
+        <AppCacheProvider>
+            <Head>
+                <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+            </Head>
+
+            <AppProviders>
+                <Component {...pageProps} className={componentClassName} />
+            </AppProviders>
+        </AppCacheProvider>,
     )
+}
+
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
 }

src/pages/_document.tsx
@@ -1,11 +1,37 @@
-import InitColorSchemeScript from '@mui/material/InitColorSchemeScript'
-import { Html, Head, Main, NextScript } from 'next/document'
+import {
+    type DocumentHeadTagsProps,
+    DocumentHeadTags,
+    documentGetInitialProps,
+} from '@mui/material-nextjs/v15-pagesRouter'
 
-export default function Document() {
+import {
+    Html,
+    Head,
+    Main,
+    NextScript,
+    type DocumentContext,
+    type DocumentProps,
+} from 'next/document'
+
+import InitColorSchemeScript from '@mui/material/InitColorSchemeScript'
+
+/**
+ * references:
+ * - [InitColorSchemeScript](https://mui.com/material-ui/customization/css-theme-variables/configuration/#next-js-pages-router)
+ * - [DocumentHeadTags](https://mui.com/material-ui/integrations/nextjs/#configuration-2)
+ */
+export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
     return (
-        <Html lang="id" data-mui-color-scheme="light">
+        <Html lang="id" className="light">
             <Head>
+                <DocumentHeadTags {...props} />
+
                 <meta charSet="utf8" />
+
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+                />
 
                 <meta
                     name="application-name"
@@ -33,12 +59,22 @@ export default function Document() {
             </Head>
 
             <body>
-                <InitColorSchemeScript attribute="data-mui-color-scheme" />
+                <InitColorSchemeScript attribute="class" />
+
                 <Main />
                 <NextScript />
             </body>
         </Html>
     )
+}
+
+/**
+ * @see https://mui.com/material-ui/integrations/nextjs/#configuration-2
+ */
+Document.getInitialProps = async (ctx: DocumentContext) => {
+    const finalProps = await documentGetInitialProps(ctx)
+
+    return finalProps
 }
 
 const TwitterMetaTags = () => (

src/providers/@statics/theme.tsx
@@ -11,7 +11,7 @@ const LinkBehavior = forwardRef<HTMLAnchorElement, LinkProps>(
 
 const THEME = createTheme({
     cssVariables: {
-        colorSchemeSelector: 'data-mui-color-scheme',
+        colorSchemeSelector: 'class',
     },
     colorSchemes: {
         light: {

src/providers/app-providers.tsx
@@ -1,20 +1,13 @@
-// assets
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 // types
 import type { ReactNode } from 'react'
 // vendors
 import { closeSnackbar, SnackbarProvider } from 'notistack'
+import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import GlobalStyles from '@mui/material/GlobalStyles'
-import { ThemeProvider } from '@mui/material/styles'
 import IconButton from '@mui/material/IconButton'
-import Close from '@mui/icons-material/Close'
-import { locale } from 'dayjs'
-import Head from 'next/head'
-import 'dayjs/locale/id'
+// icons
+import CloseIcon from '@mui/icons-material/Close'
 // providers
 import { AuthProvider } from '@/providers/Auth'
 import { SWRProvider } from './swr'
@@ -22,7 +15,9 @@ import { SWRProvider } from './swr'
 import useRedirectIfBrowserIsUnsupported from '@/hooks/useRedirectIfBrowserIsUnsupported'
 // statics
 import THEME from '@/providers/@statics/theme'
-
+//
+import { locale } from 'dayjs'
+import 'dayjs/locale/id'
 locale('id')
 
 /**
@@ -70,14 +65,6 @@ export function AppProviders({ children }: { children: ReactNode }) {
             />
             <CssBaseline />
 
-            <Head>
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-                />
-                <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
-            </Head>
-
             <SnackbarProvider
                 anchorOrigin={{
                     vertical: 'top',
@@ -85,7 +72,7 @@ export function AppProviders({ children }: { children: ReactNode }) {
                 }}
                 action={key => (
                     <IconButton size="small" onClick={() => closeSnackbar(key)}>
-                        <Close />
+                        <CloseIcon />
                     </IconButton>
                 )}
                 maxSnack={7}

commit 5574062997598c3289f2751fec93d5ec69e1c35b

hotfix font optimization not work well
caused by #700, font is not applied on the components that rendered outside the page (pages router) like mui's `<Menu />` that using `popover` and portal.

src/pages/_app.tsx
@@ -32,7 +32,7 @@ export default function App(props: AppPropsWithLayout) {
     const getLayout = Component.getLayout ?? (page => page)
 
     return getLayout(
-        <AppCacheProvider>
+        <AppCacheProvider {...props}>
             <Head>
                 <meta
                     name="viewport"
@@ -40,14 +40,20 @@ export default function App(props: AppPropsWithLayout) {
                 />
 
                 <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+
+                <style>
+                    {`
+                        :root {
+                            --font-roboto: ${roboto.style.fontFamily};
+                        }
+                    `}
+                </style>
             </Head>
 
             <RedirectIfBrowserIsUnsupported />
 
             <AppProviders>
-                <div className={roboto.className}>
-                    <Component {...pageProps} />
-                </div>
+                <Component {...pageProps} />
             </AppProviders>
         </AppCacheProvider>,
     )