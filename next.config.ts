4affec1fc2443debfb1b1fe30bcb022545b781db
Zain🍕 <zainadam.id@gmail.com>
2025-08-12 17:22:43 +0800

    fix sw bad-precaching-response

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,27 @@ const nextConfig: NextConfig = {
 const withSerwist = withSerwistInit({
     swSrc: 'src/sw.ts',
     swDest: 'public/sw.js',
-    exclude: [/public\/sw.js/, /dynamic-css-manifest.json/], // explicitly excluding the file causing problem
+
+    /**
+     * @see https://github.com/serwist/serwist/discussions/228
+     */
+    exclude: [
+        // https://github.com/shadowwalker/next-pwa/issues/424#issuecomment-1332258575
+        ({ asset }) => {
+            // Add here any file that fails pre-caching
+            const excludeList = [
+                // Default Serwist https://serwist.pages.dev/docs/next/configuring/exclude
+                /\.map$/,
+                /^manifest.*\.js$/,
+                /^server\//,
+                /^(((app-)?build-manifest|react-loadable-manifest|dynamic-css-manifest)\.json)$/,
+            ]
+            if (excludeList.some(r => r.test(asset.name))) {
+                return true
+            }
+            return false
+        },
+    ],
     reloadOnOnline: false,
 })
 
