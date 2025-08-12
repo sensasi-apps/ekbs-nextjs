sensasi-apps/ekbs-nextjs

commit a9d9331407acea80a9173bc17ac29f502245e397

adjust `manifest.webmanifest` for both app and pages router

public/pages-router/manifest.webmanifest
@@ -1,0 +1,21 @@
+{
+    "name": "Koperasi Belayan Sejahtera Elektronik",
+    "short_name": "EKBS",
+    "display": "standalone",
+    "start_url": "/",
+    "background_color": "#121212",
+    "icons": [
+        {
+            "src": "/assets/pwa-icons/white-green.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+        },
+        {
+            "src": "/favicon192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+        }
+    ]
+}

src/app/manifest.webmanifest
@@ -1,0 +1,21 @@
+{
+    "name": "Koperasi Belayan Sejahtera Elektronik",
+    "short_name": "EKBS",
+    "display": "standalone",
+    "start_url": "/",
+    "background_color": "#121212",
+    "icons": [
+        {
+            "src": "/assets/pwa-icons/white-green.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+        },
+        {
+            "src": "/favicon192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "any"
+        }
+    ]
+}

src/pages/_document.tsx
@@ -47,7 +47,10 @@ export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
                     href="/assets/pwa-icons/white-green.png"
                 />
 
-                <link rel="manifest" href="/manifest.webmanifest" />
+                <link
+                    rel="manifest"
+                    href="/pages-router/manifest.webmanifest"
+                />
 
                 <TwitterMetaTags />
                 <FacebookMetaTags />

public/manifest.webmanifest
@@ -1,21 +1,0 @@
-{
-    "name": "Koperasi Belayan Sejahtera Elektronik",
-    "short_name": "EKBS",
-    "display": "standalone",
-    "start_url": "/",
-    "background_color": "#121212",
-    "icons": [
-        {
-            "src": "/assets/pwa-icons/white-green.svg",
-            "sizes": "48x48 72x72 96x96 128x128 192x192 256x256 512x512",
-            "type": "image/svg+xml",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/assets/pwa-icons/green-transparent.svg",
-            "sizes": "48x48 72x72 96x96 128x128 192x192 256x256 512x512",
-            "type": "image/svg+xml",
-            "purpose": "any"
-        }
-    ]
-}

src/app/layout.tsx
@@ -14,11 +14,6 @@ const roboto = Roboto({
     variable: '--font-roboto',
 })
 
-export const metadata: Metadata = {
-    title: process.env.NEXT_PUBLIC_APP_NAME,
-    description: process.env.NEXT_PUBLIC_APP_NAME,
-}
-
 export default function RootLayout({
     children,
 }: {
@@ -37,4 +32,44 @@ export default function RootLayout({
             </body>
         </html>
     )
+}
+
+const APP_NAME = 'EKBS'
+const APP_DEFAULT_TITLE = process.env.NEXT_PUBLIC_APP_NAME ?? ''
+const APP_TITLE_TEMPLATE = `%s - ${process.env.NEXT_PUBLIC_APP_NAME}`
+const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_NAME
+
+export const metadata: Metadata = {
+    applicationName: APP_NAME,
+    title: {
+        default: APP_DEFAULT_TITLE,
+        template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: 'default',
+        title: APP_DEFAULT_TITLE,
+        // startUpImage: [],
+    },
+    formatDetection: {
+        telephone: false,
+    },
+    openGraph: {
+        type: 'website',
+        siteName: APP_NAME,
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
+    twitter: {
+        card: 'summary',
+        title: {
+            default: APP_DEFAULT_TITLE,
+            template: APP_TITLE_TEMPLATE,
+        },
+        description: APP_DESCRIPTION,
+    },
 }

commit 6cd0816c99570fca36c02a518f0db3fa3bc39bed

fix redundant webmanifest caused by a9d9331407acea80a9173bc17ac29f502245e397

public/pages-router/manifest.webmanifest
@@ -1,21 +1,0 @@
-{
-    "name": "Koperasi Belayan Sejahtera Elektronik",
-    "short_name": "EKBS",
-    "display": "standalone",
-    "start_url": "/",
-    "background_color": "#121212",
-    "icons": [
-        {
-            "src": "/assets/pwa-icons/white-green.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "maskable"
-        },
-        {
-            "src": "/favicon192.png",
-            "sizes": "192x192",
-            "type": "image/png",
-            "purpose": "any"
-        }
-    ]
-}

src/app/manifest.webmanifest
@@ -12,7 +12,7 @@
             "purpose": "maskable"
         },
         {
-            "src": "/favicon192.png",
+            "src": "/assets/pwa-icons/green-transparent.png",
             "sizes": "192x192",
             "type": "image/png",
             "purpose": "any"

src/pages/_document.tsx
@@ -47,10 +47,7 @@ export default function Document(props: DocumentProps & DocumentHeadTagsProps) {
                     href="/assets/pwa-icons/white-green.png"
                 />
 
-                <link
-                    rel="manifest"
-                    href="/pages-router/manifest.webmanifest"
-                />
+                <link rel="manifest" href="/manifest.webmanifest" />
 
                 <TwitterMetaTags />
                 <FacebookMetaTags />