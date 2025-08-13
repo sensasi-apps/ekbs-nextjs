a19ecc65ca913cae0ffe6cc4fbfd5fd9b37367d4
Zain🍕 <zainadam.id@gmail.com>
2025-08-13 16:03:47 +0800

    merge `userHasPermission` function

--- src/hooks/use-is-auth-has-permission.ts
+++ src/hooks/use-is-auth-has-permission.ts
@@ -1,10 +1,32 @@
 import type { Permission } from '@/types/permission'
-import userHasPermission from '@/providers/Auth/userHasPermission'
 import useAuthInfo from './use-auth-info'
+import type AuthInfo from '@/features/user--auth/types/auth-info'
+import Role from '@/enums/Role'
 
 export default function useIsAuthHasPermission() {
     const authInfo = useAuthInfo()
 
     return (permissionName: Permission | Permission[]) =>
-        userHasPermission(permissionName, authInfo)
+        isUserHasPermission(permissionName, authInfo)
+}
+
+function isUserHasPermission(
+    permissionName: Permission | Permission[],
+    userParam: AuthInfo | undefined,
+) {
+    if (!userParam) return false
+
+    if (userParam.role_names?.includes(Role.SUPERMAN)) {
+        return true
+    }
+
+    if (permissionName instanceof Array) {
+        return (
+            permissionName.findIndex(p =>
+                userParam.permission_names?.includes(p),
+            ) !== -1
+        )
+    }
+
+    return userParam.permission_names?.includes(permissionName)
 }
