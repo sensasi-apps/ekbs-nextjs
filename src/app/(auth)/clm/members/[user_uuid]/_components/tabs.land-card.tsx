184011d4767cefaf7567d4ff029175a1c3b3de73
Zain🍕 <zainadam.id@gmail.com>
2025-09-03 22:25:59 +0800

    add member land create page

--- src/app/(auth)/clm/members/[user_uuid]/_components/tabs.land-card.tsx
+++ src/app/(auth)/clm/members/[user_uuid]/_components/tabs.land-card.tsx
@@ -1,43 +1,35 @@
 // materials
-import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardActionArea from '@mui/material/CardActionArea'
 import CardContent from '@mui/material/CardContent'
 import Typography from '@mui/material/Typography'
 // icons
+import AssuredWorkloadIcon from '@mui/icons-material/AssuredWorkload'
+import CalendarTodayIcon from '@mui/icons-material/CalendarToday'
 import ForestIcon from '@mui/icons-material/Forest'
 import GroupsIcon from '@mui/icons-material/Groups'
+import HomeIcon from '@mui/icons-material/Home'
+import EditNoteIcon from '@mui/icons-material/EditNote'
 // components
-import type Land from '@/modules/clm/types/orms/land'
 import ChipSmall from '@/components/ChipSmall'
+// modules
+import type Land from '@/modules/clm/types/orms/land'
 // utils
 import shortUuid from '@/utils/short-uuid'
+import toDmy from '@/utils/to-dmy'
+import FlexBox from '@/components/flex-box'
 
 export default function LandCard({ land }: { land: Land }) {
     return (
         <Card>
             <CardActionArea>
                 <CardContent sx={{ p: 3 }}>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        justifyContent="space-between">
-                        <Box display="flex" alignItems="center" gap={1.5}>
-                            <ForestIcon />
-
-                            <Typography
-                                variant="h4"
-                                fontWeight="bold"
-                                component="div">
-                                {land.n_area_hectares} Ha
-                            </Typography>
-
-                            <ChipSmall
-                                label={shortUuid(land.uuid)}
-                                color="info"
-                                variant="outlined"
-                            />
-                        </Box>
+                    <FlexBox mb={1} justifyContent="space-between">
+                        <ChipSmall
+                            label={shortUuid(land.uuid)}
+                            color="info"
+                            variant="outlined"
+                        />
 
                         <ChipSmall
                             label={
@@ -51,41 +43,53 @@ export default function LandCard({ land }: { land: Land }) {
                             color="error"
                             variant="outlined"
                         />
-                    </Box>
+                    </FlexBox>
 
-                    <Typography variant="caption" component="div" color="gray">
-                        {land.uuid}
-                    </Typography>
+                    <FlexBox mb={2}>
+                        <ForestIcon />
 
-                    {land.farmer_group && (
-                        <Box
-                            display="flex"
-                            gap={1}
-                            alignItems="center"
-                            sx={{
-                                opacity: 0.7,
-                            }}>
-                            <GroupsIcon color="warning" />
-
-                            <Typography component="div" color="warning">
-                                {land.farmer_group.name}
-                            </Typography>
-                        </Box>
-                    )}
-
-                    {land.note && (
-                        <Typography variant="body2" component="div">
-                            {land.note}
+                        <Typography
+                            variant="h4"
+                            fontWeight="bold"
+                            component="div">
+                            {land.n_area_hectares} Ha
                         </Typography>
-                    )}
+                    </FlexBox>
 
-                    {land.address.detail && (
-                        <Typography variant="body2" component="div">
-                            {land.address.detail}
-                        </Typography>
-                    )}
+                    <Info
+                        Icon={CalendarTodayIcon}
+                        text={toDmy(land.planted_at)}
+                    />
+
+                    <Info Icon={AssuredWorkloadIcon} text={land.rea_land_id} />
+
+                    <Info Icon={GroupsIcon} text={land.farmer_group?.name} />
+
+                    <Info
+                        Icon={HomeIcon}
+                        text={`${land.address.detail} (${land.address.region.name})`}
+                    />
+
+                    <Info Icon={EditNoteIcon} text={land.note} />
                 </CardContent>
             </CardActionArea>
         </Card>
+    )
+}
+
+function Info({ Icon, text }: { Icon: typeof HomeIcon; text?: string }) {
+    if (!text) return null
+
+    return (
+        <Typography
+            variant="body2"
+            component="div"
+            display="flex"
+            gap={1}
+            alignItems="middle"
+            color="textDisabled">
+            <Icon fontSize="small" />
+            {text}
+        </Typography>
     )
 }
