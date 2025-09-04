25f3ddadbe93e3f8b3ee029f6d469b81ad736ea4
Zain🍕 <zainadam.id@gmail.com>
2025-09-04 23:44:05 +0800

    improve value and error

--- src/components/formik-fields/region-id-autocomplete.tsx
+++ src/components/formik-fields/region-id-autocomplete.tsx
@@ -1,30 +1,42 @@
 'use client'
 
-import Autocomplete from '@/components/Inputs/Autocomplete'
+// vendors
 import { useFormikContext } from 'formik'
 import { useState } from 'react'
+// materials
+import FormHelperText from '@mui/material/FormHelperText'
+// components
+import Autocomplete from '@/components/Inputs/Autocomplete'
 
 export default function RegionIdAutocomplete() {
-    const { isSubmitting, setFieldValue } = useFormikContext()
-    const [innerValue, setInnerValue] = useState(null)
+    const { isSubmitting, setFieldValue, status, errors } = useFormikContext<{
+        region_id: number
+    }>()
+    const [innerValue, setInnerValue] = useState(status?.region ?? null)
 
     return (
-        <Autocomplete
-            required
-            disabled={isSubmitting}
-            margin="dense"
-            value={innerValue}
-            onChange={(_, value) => {
-                if (value) {
-                    setInnerValue(value)
-                    setFieldValue('region_id', `${value.id}`)
-                } else {
-                    setInnerValue(null)
-                    setFieldValue('region_id', null)
-                }
-            }}
-            endpoint="/select2/administrative-regions"
-            label="Wilayah Administratif"
-        />
+        <>
+            <Autocomplete
+                required
+                disabled={isSubmitting}
+                margin="dense"
+                value={innerValue}
+                onChange={(_, value) => {
+                    if (value) {
+                        setInnerValue(value)
+                        setFieldValue('region_id', `${value.id}`)
+                    } else {
+                        setInnerValue(null)
+                        setFieldValue('region_id', null)
+                    }
+                }}
+                endpoint="/select2/administrative-regions"
+                label="Wilayah Administratif"
+            />
+
+            {errors['region_id'] && (
+                <FormHelperText error>{errors['region_id']}</FormHelperText>
+            )}
+        </>
     )
 }
