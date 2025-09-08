sensasi-apps/ekbs-nextjs

commit b90436a96e92411d4583ec8e2623cc67f80810f3

remove `<UserWithDetailsProvider />`

src/app/(auth)/systems/users/[uuid]/_parts/dialog-form-with-fab.tsx
@@ -6,21 +6,19 @@ import Fab from '@mui/material/Fab'
 
 import PersonAddIcon from '@mui/icons-material/PersonAdd'
 
-import UserForm from '@/app/(auth)/systems/users/[[...uuid]]/_parts/user-form'
+import UserForm from '@/app/(auth)/systems/users/[uuid]/_parts/user-form'
 
 import useFormData from '@/providers/FormData'
-import useUserWithDetails from '@/app/(auth)/systems/users/[[...uuid]]/_parts/user-with-details-provider'
 
-const UserDialogFormWithFab = () => {
-    const { isLoading } = useUserWithDetails()
+export default function UserDialogFormWithFab() {
     const { handleCreate, isDataNotUndefined, isNew } = useFormData()
+
+    const text = isNew ? 'Buat akun baru' : 'Perbarui data akun'
 
     return (
         <>
             <Dialog maxWidth="xs" open={isDataNotUndefined}>
-                <DialogTitle>
-                    {isNew ? 'Buat akun baru' : 'Perbarui data akun'}
-                </DialogTitle>
+                <DialogTitle>{text}</DialogTitle>
                 <DialogContent>
                     <UserForm />
                 </DialogContent>
@@ -28,17 +26,15 @@ const UserDialogFormWithFab = () => {
 
             <Box position="fixed" bottom={16} right={16} zIndex={1}>
                 <Fab
-                    disabled={isDataNotUndefined || isLoading}
+                    disabled={isDataNotUndefined}
                     onClick={() => {
                         handleCreate()
                     }}
                     color="success"
-                    aria-label="buat akun baru">
+                    aria-label={text}>
                     <PersonAddIcon />
                 </Fab>
             </Box>
         </>
     )
 }
-
-export default UserDialogFormWithFab

src/app/(auth)/systems/users/[uuid]/page.tsx
@@ -1,0 +1,208 @@
+'use client'
+
+// materials
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardHeader, { type CardHeaderProps } from '@mui/material/CardHeader'
+import Fade from '@mui/material/Fade'
+import Typography from '@mui/material/Typography'
+// mui labs
+import Masonry from '@mui/lab/Masonry'
+// page components
+import { getRoleIconByIdName } from '@/components/User/RoleChips'
+import { UserDetailCrud } from '@/components/User/Detail/Crud'
+import UserAddressesCrudBox from '@/components/User/Address/crud-box'
+import UserBankAccsCrudBox from '@/components/User/BankAccs/crud-box'
+import UserDriversCrudBox from '@/modules/user/components/user-driver-crud-box'
+import UserEmployeeCrud from '@/components/User/Employee/Crud'
+import UserLandsCrud from '@/modules/user/components/user-land-crud'
+import UserMemberCrudCard from '@/components/User/Member/crud-card'
+import UserSocialsCrudBox from '@/components/User/Socials/CrudBox'
+import UserVehiclesCrudBox from '@/components/User/Vehicles/crud-box'
+// utils
+import Role from '@/enums/role'
+import UserCard from '@/app/(auth)/systems/users/[uuid]/_parts/user-card'
+// hooks
+import { isUserHasRole } from '@/hooks/use-is-auth-has-role'
+import useUserDetailSwr from '@/modules/user/hooks/use-user-detail-swr'
+import LoadingCenter from '@/components/loading-center'
+import BackButton from '@/components/back-button'
+import { FormDataProvider } from '@/providers/FormData'
+
+export default function Page() {
+    const { data: userWithDetails, isLoading } = useUserDetailSwr()
+
+    if (!userWithDetails) return <LoadingCenter />
+
+    const {
+        addresses = [],
+        uuid,
+        socials = [],
+        employee,
+        driver,
+        drivers = [],
+        vehicles = [],
+    } = userWithDetails
+
+    return (
+        <>
+            <BackButton />
+
+            <Box display="flex" flexDirection="column" gap={3}>
+                <FormDataProvider>
+                    <UserCard />
+                </FormDataProvider>
+
+                <UserDetailCrud />
+
+                <Masonry
+                    sx={{
+                        m: 0,
+                    }}
+                    columns={{
+                        md: 2,
+                        sm: 1,
+                        xs: 1,
+                    }}
+                    spacing={2}>
+                    <Card>
+                        <CardContent>
+                            <UserSocialsCrudBox
+                                userUuid={uuid}
+                                data={socials}
+                                isLoading={isLoading}
+                            />
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <UserAddressesCrudBox
+                                userUuid={uuid}
+                                data={addresses}
+                                isLoading={isLoading}
+                            />
+                        </CardContent>
+                    </Card>
+
+                    <Card>
+                        <CardContent>
+                            <UserBankAccsCrudBox />
+                        </CardContent>
+                    </Card>
+
+                    <Fade
+                        in={isUserHasRole(Role.EMPLOYEE, userWithDetails)}
+                        unmountOnExit
+                        exit={false}>
+                        <Card>
+                            <CardHeader
+                                avatar={getRoleIconByIdName('karyawan')}
+                                title="Kepegawaian"
+                                titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
+                            />
+                            <CardContent sx={PT_0_SX}>
+                                <UserEmployeeCrud
+                                    userUuid={uuid}
+                                    data={employee}
+                                />
+                            </CardContent>
+                        </Card>
+                    </Fade>
+
+                    <Fade
+                        in={isUserHasRole(Role.MEMBER, userWithDetails)}
+                        unmountOnExit
+                        exit={false}>
+                        <div>
+                            <UserMemberCrudCard />
+                        </div>
+                    </Fade>
+
+                    <Fade
+                        in={isUserHasRole(Role.FARMER, userWithDetails)}
+                        unmountOnExit
+                        exit={false}>
+                        <Card>
+                            <CardHeader
+                                avatar={getRoleIconByIdName('petani')}
+                                title="Daftar Kebun"
+                                titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
+                            />
+                            <CardContent sx={PT_0_SX}>
+                                <UserLandsCrud />
+                            </CardContent>
+                        </Card>
+                    </Fade>
+
+                    <Fade
+                        in={isUserHasRole(Role.DRIVER, userWithDetails)}
+                        unmountOnExit
+                        exit={false}>
+                        <Card>
+                            <CardHeader
+                                avatar={getRoleIconByIdName('pengangkut')}
+                                title="Daftar Pengemudi"
+                                titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
+                            />
+                            <CardContent sx={PT_0_SX}>
+                                <UserDriversCrudBox
+                                    courierUserUuid={uuid}
+                                    data={drivers}
+                                />
+                            </CardContent>
+                        </Card>
+                    </Fade>
+
+                    <Fade
+                        in={isUserHasRole(Role.DRIVER, userWithDetails)}
+                        unmountOnExit
+                        exit={false}>
+                        <Card>
+                            <CardHeader
+                                avatar={getRoleIconByIdName('pengangkut')}
+                                title="Daftar Kendaraan Pengangkut"
+                                titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
+                            />
+                            <CardContent sx={PT_0_SX}>
+                                <UserVehiclesCrudBox
+                                    data={vehicles}
+                                    courierUserUuid={uuid}
+                                />
+                            </CardContent>
+                        </Card>
+                    </Fade>
+
+                    <Fade
+                        in={isUserHasRole(Role.DRIVER, userWithDetails)}
+                        unmountOnExit
+                        exit={false}>
+                        <Card>
+                            <CardHeader
+                                avatar={getRoleIconByIdName('pengemudi')}
+                                title="Informasi Pengemudi"
+                                titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
+                            />
+                            <CardContent sx={PT_0_SX}>
+                                <Typography variant="caption" color="GrayText">
+                                    No. SIM:
+                                </Typography>
+                                <Typography>
+                                    {driver?.license_number}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    </Fade>
+                </Masonry>
+            </Box>
+        </>
+    )
+}
+
+const TITLE_TYPOGRAPHY_PROPS: CardHeaderProps['titleTypographyProps'] = {
+    variant: 'body1',
+    fontWeight: 'bold',
+}
+
+const PT_0_SX = { pt: 0 }

src/app/(auth)/systems/users/page.tsx
@@ -1,23 +1,19 @@
 'use client'
 
 // vendors
-import { useParams, useRouter, useSearchParams } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import Box from '@mui/material/Box'
-import Collapse from '@mui/material/Collapse'
 import Grid from '@mui/material/Grid'
 // components
 import { FormDataProvider } from '@/providers/FormData'
 import Datatable, { type DatatableProps } from '@/components/Datatable'
 import UserRoleChips from '@/components/User/RoleChips'
 // parts
-import { UserSummaryBox } from '@/app/(auth)/systems/users/[[...uuid]]/_parts/summary-box'
-import { UserWithDetailsProvider } from '@/app/(auth)/systems/users/[[...uuid]]/_parts/user-with-details-provider'
-import UserDialogFormWithFab from '@/app/(auth)/systems/users/[[...uuid]]/_parts/dialog-form-with-fab'
-import UsersMainPageContent from '@/app/(auth)/systems/users/[[...uuid]]/_parts/main-page-content'
+import { UserSummaryBox } from '@/app/(auth)/systems/users/[uuid]/_parts/summary-box'
+import UserDialogFormWithFab from '@/app/(auth)/systems/users/[uuid]/_parts/dialog-form-with-fab'
 
 export default function Page() {
     const { push } = useRouter()
-    const params = useParams()
 
     const searchParams = useSearchParams()
     const selectedRole = searchParams?.get('role') ?? ''
@@ -34,15 +30,9 @@ export default function Page() {
                     },
                 }}>
                 <Grid size={{ xs: 12, md: 8 }}>
-                    <UserWithDetailsProvider>
-                        <FormDataProvider>
-                            <Collapse in={Boolean(params?.uuid)} unmountOnExit>
-                                <UsersMainPageContent />
-                            </Collapse>
-
-                            <UserDialogFormWithFab />
-                        </FormDataProvider>
-                    </UserWithDetailsProvider>
+                    <FormDataProvider>
+                        <UserDialogFormWithFab />
+                    </FormDataProvider>
 
                     <Datatable
                         apiUrl="users/get-datatable-data"

src/app/(auth)/systems/users/[[...uuid]]/_parts/main-page-content.tsx
@@ -1,207 +1,0 @@
-// vendors
-import { useRouter } from 'next/navigation'
-// materials
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import CardContent from '@mui/material/CardContent'
-import CardHeader, { type CardHeaderProps } from '@mui/material/CardHeader'
-import Fade from '@mui/material/Fade'
-import Typography from '@mui/material/Typography'
-// mui labs
-import Masonry from '@mui/lab/Masonry'
-// page components
-import { getRoleIconByIdName } from '@/components/User/RoleChips'
-import { UserDetailCrud } from '@/components/User/Detail/Crud'
-import UserAddressesCrudBox from '@/components/User/Address/crud-box'
-import UserBankAccsCrudBox from '@/components/User/BankAccs/crud-box'
-import UserDriversCrudBox from '@/modules/user/components/user-driver-crud-box'
-import UserEmployeeCrud from '@/components/User/Employee/Crud'
-import UserLandsCrud from '@/modules/user/components/user-land-crud'
-import UserMemberCrudCard from '@/components/User/Member/crud-card'
-import UserSocialsCrudBox from '@/components/User/Socials/CrudBox'
-import UserVehiclesCrudBox from '@/components/User/Vehicles/crud-box'
-// utils
-import useUserWithDetails from '@/app/(auth)/systems/users/[[...uuid]]/_parts/user-with-details-provider'
-import Role from '@/enums/role'
-import UserCard from '@/app/(auth)/systems/users/[[...uuid]]/_parts/user-card'
-// hooks
-import { isUserHasRole } from '@/hooks/use-is-auth-has-role'
-import { useSearchParams } from 'next/navigation'
-
-export default function UsersMainPageContent() {
-    const { replace } = useRouter()
-    const searchParams = useSearchParams()
-
-    const {
-        data: userWithDetails = {},
-        error,
-        isLoading,
-    } = useUserWithDetails()
-
-    if (error?.response?.status === 404) {
-        replace('/systems/users?role=' + searchParams?.get('role'))
-    }
-
-    const {
-        addresses,
-        uuid,
-        socials,
-        employee,
-        // member,
-        driver,
-
-        // lands,
-        drivers,
-        vehicles,
-    } = userWithDetails
-
-    return (
-        <Box display="flex" flexDirection="column" gap={3}>
-            <UserCard />
-
-            <UserDetailCrud />
-
-            <Masonry
-                sx={{
-                    m: 0,
-                }}
-                columns={{
-                    md: 2,
-                    sm: 1,
-                    xs: 1,
-                }}
-                spacing={2}>
-                <Card>
-                    <CardContent>
-                        <UserSocialsCrudBox
-                            userUuid={uuid}
-                            data={socials}
-                            isLoading={isLoading}
-                        />
-                    </CardContent>
-                </Card>
-
-                <Card>
-                    <CardContent>
-                        <UserAddressesCrudBox
-                            userUuid={uuid}
-                            data={addresses}
-                            isLoading={isLoading}
-                        />
-                    </CardContent>
-                </Card>
-
-                <Card>
-                    <CardContent>
-                        <UserBankAccsCrudBox />
-                    </CardContent>
-                </Card>
-
-                <Fade
-                    in={isUserHasRole(Role.EMPLOYEE, userWithDetails)}
-                    unmountOnExit
-                    exit={false}>
-                    <Card>
-                        <CardHeader
-                            avatar={getRoleIconByIdName('karyawan')}
-                            title="Kepegawaian"
-                            titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
-                        />
-                        <CardContent sx={PT_0_SX}>
-                            <UserEmployeeCrud userUuid={uuid} data={employee} />
-                        </CardContent>
-                    </Card>
-                </Fade>
-
-                <Fade
-                    in={isUserHasRole(Role.MEMBER, userWithDetails)}
-                    unmountOnExit
-                    exit={false}>
-                    <div>
-                        <UserMemberCrudCard />
-                    </div>
-                </Fade>
-
-                <Fade
-                    in={isUserHasRole(Role.FARMER, userWithDetails)}
-                    unmountOnExit
-                    exit={false}>
-                    <Card>
-                        <CardHeader
-                            avatar={getRoleIconByIdName('petani')}
-                            title="Daftar Kebun"
-                            titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
-                        />
-                        <CardContent sx={PT_0_SX}>
-                            <UserLandsCrud />
-                        </CardContent>
-                    </Card>
-                </Fade>
-
-                <Fade
-                    in={isUserHasRole(Role.DRIVER, userWithDetails)}
-                    unmountOnExit
-                    exit={false}>
-                    <Card>
-                        <CardHeader
-                            avatar={getRoleIconByIdName('pengangkut')}
-                            title="Daftar Pengemudi"
-                            titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
-                        />
-                        <CardContent sx={PT_0_SX}>
-                            <UserDriversCrudBox
-                                courierUserUuid={uuid}
-                                data={drivers}
-                            />
-                        </CardContent>
-                    </Card>
-                </Fade>
-
-                <Fade
-                    in={isUserHasRole(Role.DRIVER, userWithDetails)}
-                    unmountOnExit
-                    exit={false}>
-                    <Card>
-                        <CardHeader
-                            avatar={getRoleIconByIdName('pengangkut')}
-                            title="Daftar Kendaraan Pengangkut"
-                            titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
-                        />
-                        <CardContent sx={PT_0_SX}>
-                            <UserVehiclesCrudBox
-                                data={vehicles}
-                                courierUserUuid={uuid}
-                            />
-                        </CardContent>
-                    </Card>
-                </Fade>
-
-                <Fade
-                    in={isUserHasRole(Role.DRIVER, userWithDetails)}
-                    unmountOnExit
-                    exit={false}>
-                    <Card>
-                        <CardHeader
-                            avatar={getRoleIconByIdName('pengemudi')}
-                            title="Informasi Pengemudi"
-                            titleTypographyProps={TITLE_TYPOGRAPHY_PROPS}
-                        />
-                        <CardContent sx={PT_0_SX}>
-                            <Typography variant="caption" color="GrayText">
-                                No. SIM:
-                            </Typography>
-                            <Typography>{driver?.license_number}</Typography>
-                        </CardContent>
-                    </Card>
-                </Fade>
-            </Masonry>
-        </Box>
-    )
-}
-
-const TITLE_TYPOGRAPHY_PROPS: CardHeaderProps['titleTypographyProps'] = {
-    variant: 'body1',
-    fontWeight: 'bold',
-}
-
-const PT_0_SX = { pt: 0 }

src/app/(auth)/systems/users/[[...uuid]]/_parts/user-with-details-provider.tsx
@@ -1,37 +1,0 @@
-import { createContext, type ReactNode, useContext } from 'react'
-import { useParams } from 'next/navigation'
-import useSWR, { type SWRResponse } from 'swr'
-//
-import axios from '@/lib/axios'
-import type User from '@/modules/user/types/orms/user'
-
-const UserWithDetailsCtx = createContext<SWRResponse>({} as SWRResponse)
-
-const userWithDetailsFetcher = (url: string) =>
-    axios.get(url).then(response => response.data)
-
-/**
- * @deprecated use formik instead
- */
-export function UserWithDetailsProvider({ children }: { children: ReactNode }) {
-    const params = useParams()
-    const uuid = params?.uuid as string | undefined
-
-    const swr = useSWR<User>(
-        uuid ? `users/${uuid}` : null,
-        userWithDetailsFetcher,
-    )
-
-    return (
-        <UserWithDetailsCtx.Provider value={swr}>
-            {children}
-        </UserWithDetailsCtx.Provider>
-    )
-}
-
-/**
- * @deprecated use formik instead
- */
-const useUserWithDetails = () => useContext(UserWithDetailsCtx)
-
-export default useUserWithDetails

src/app/(auth)/systems/users/[uuid]/_parts/user-card.tsx
@@ -1,5 +1,3 @@
-// vendors
-import { useRouter } from 'next/navigation'
 // materials
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
@@ -9,11 +7,9 @@ import Link from '@mui/material/Link'
 import Skeleton from '@mui/material/Skeleton'
 import Typography from '@mui/material/Typography'
 // icons-materials
-import Close from '@mui/icons-material/Close'
 import Edit from '@mui/icons-material/Edit'
 import OpenInNewIcon from '@mui/icons-material/OpenInNew'
 // global components
-import IconButton from '@/components/IconButton'
 import LoadingCenter from '@/components/loading-center'
 import ChipSmall from '@/components/ChipSmall'
 import FlexBox from '@/components/flex-box'
@@ -25,18 +21,17 @@ import UserRoleChips from '@/components/User/RoleChips'
 import useFormData from '@/providers/FormData'
 //
 import RolesAndPermissionButtonAndDialogForm from './roles-and-permission-button-and-dialog-form'
-import useUserWithDetails from './user-with-details-provider'
+import useUserDetailSwr from '@/modules/user/hooks/use-user-detail-swr'
 
 export default function UserCard() {
-    const { back } = useRouter()
     const { handleEdit } = useFormData()
-    const { data: userWithDetails, isLoading } = useUserWithDetails()
+    const { data: userWithDetails, isLoading } = useUserDetailSwr()
 
     const { name, role_names_id, id, email, uuid } = userWithDetails || {}
 
     const handleEditClick = () => handleEdit(userWithDetails)
 
-    if (isLoading) return <LoadingCenter />
+    if (isLoading || !userWithDetails) return <LoadingCenter />
 
     return (
         <Card>
@@ -53,22 +48,13 @@ export default function UserCard() {
                             variant="outlined"
                         />
                     </FlexBox>
-
-                    <IconButton
-                        title="Tutup"
-                        icon={Close}
-                        color="warning"
-                        onClick={() => {
-                            back()
-                        }}
-                    />
                 </Box>
 
                 <Typography variant="caption" color="GrayText">
                     {isLoading ? (
                         <Skeleton />
                     ) : (
-                        email || <i>E-mail untuk akun belum ditambahkan</i>
+                        (email ?? <i>E-mail untuk akun belum ditambahkan</i>)
                     )}
                 </Typography>
 
@@ -90,7 +76,10 @@ export default function UserCard() {
                                 mb: 0.3,
                             },
                         }}>
-                        <UserRoleChips data={role_names_id} size="small" />
+                        <UserRoleChips
+                            data={role_names_id ?? []}
+                            size="small"
+                        />
                     </Box>
                 </Box>
 

commit 208522aa2650e94983f1305c5c2f9072e28a7092

fix edit form is not show and refactor
caused by b90436a96e92411d4583ec8e2623cc67f80810f3 in #752

src/app/(auth)/systems/users/[uuid]/_parts/dialog-form-with-fab.tsx
@@ -1,40 +1,0 @@
-import Box from '@mui/material/Box'
-import Dialog from '@mui/material/Dialog'
-import DialogContent from '@mui/material/DialogContent'
-import DialogTitle from '@mui/material/DialogTitle'
-import Fab from '@mui/material/Fab'
-
-import PersonAddIcon from '@mui/icons-material/PersonAdd'
-
-import UserForm from '@/app/(auth)/systems/users/[uuid]/_parts/user-form'
-
-import useFormData from '@/providers/FormData'
-
-export default function UserDialogFormWithFab() {
-    const { handleCreate, isDataNotUndefined, isNew } = useFormData()
-
-    const text = isNew ? 'Buat akun baru' : 'Perbarui data akun'
-
-    return (
-        <>
-            <Dialog maxWidth="xs" open={isDataNotUndefined}>
-                <DialogTitle>{text}</DialogTitle>
-                <DialogContent>
-                    <UserForm />
-                </DialogContent>
-            </Dialog>
-
-            <Box position="fixed" bottom={16} right={16} zIndex={1}>
-                <Fab
-                    disabled={isDataNotUndefined}
-                    onClick={() => {
-                        handleCreate()
-                    }}
-                    color="success"
-                    aria-label={text}>
-                    <PersonAddIcon />
-                </Fab>
-            </Box>
-        </>
-    )
-}

src/app/(auth)/systems/users/[uuid]/page.tsx
@@ -20,15 +20,18 @@ import UserLandsCrud from '@/modules/user/components/user-land-crud'
 import UserMemberCrudCard from '@/components/User/Member/crud-card'
 import UserSocialsCrudBox from '@/components/User/Socials/CrudBox'
 import UserVehiclesCrudBox from '@/components/User/Vehicles/crud-box'
+// components
+import BackButton from '@/components/back-button'
+import LoadingCenter from '@/components/loading-center'
 // utils
 import Role from '@/enums/role'
-import UserCard from '@/app/(auth)/systems/users/[uuid]/_parts/user-card'
 // hooks
 import { isUserHasRole } from '@/hooks/use-is-auth-has-role'
-import useUserDetailSwr from '@/modules/user/hooks/use-user-detail-swr'
-import LoadingCenter from '@/components/loading-center'
-import BackButton from '@/components/back-button'
 import { FormDataProvider } from '@/providers/FormData'
+// modules
+import useUserDetailSwr from '@/modules/user/hooks/use-user-detail-swr'
+import UserCard from '@/app/(auth)/systems/users/[uuid]/_parts/user-card'
+import UserFormDialog from '@/modules/user/components/user-form-dialog'
 
 export default function Page() {
     const { data: userWithDetails, isLoading } = useUserDetailSwr()
@@ -51,6 +54,8 @@ export default function Page() {
 
             <Box display="flex" flexDirection="column" gap={3}>
                 <FormDataProvider>
+                    <UserFormDialog />
+
                     <UserCard />
                 </FormDataProvider>
 

src/app/(auth)/systems/users/page.tsx
@@ -5,12 +5,15 @@ import { useRouter, useSearchParams } from 'next/navigation'
 import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 // components
-import { FormDataProvider } from '@/providers/FormData'
+import Fab from '@/components/Fab'
+import useFormData, { FormDataProvider } from '@/providers/FormData'
 import Datatable, { type DatatableProps } from '@/components/Datatable'
 import UserRoleChips from '@/components/User/RoleChips'
 // parts
 import { UserSummaryBox } from '@/app/(auth)/systems/users/[uuid]/_parts/summary-box'
-import UserDialogFormWithFab from '@/app/(auth)/systems/users/[uuid]/_parts/dialog-form-with-fab'
+import UserFormDialog from '@/modules/user/components/user-form-dialog'
+// icons
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
 
 export default function Page() {
     const { push } = useRouter()
@@ -31,7 +34,7 @@ export default function Page() {
                 }}>
                 <Grid size={{ xs: 12, md: 8 }}>
                     <FormDataProvider>
-                        <UserDialogFormWithFab />
+                        <UserFormDialogWithFab />
                     </FormDataProvider>
 
                     <Datatable
@@ -61,6 +64,26 @@ export default function Page() {
                     <UserSummaryBox />
                 </Grid>
             </Grid>
+        </>
+    )
+}
+
+function UserFormDialogWithFab() {
+    const { handleCreate, isDataNotUndefined } = useFormData()
+
+    return (
+        <>
+            <UserFormDialog />
+
+            <Fab
+                title="Buat pengguna baru"
+                in={!isDataNotUndefined}
+                onClick={() => {
+                    handleCreate()
+                }}
+                color="success">
+                <PersonAddIcon />
+            </Fab>
         </>
     )
 }

src/modules/user/components/user-form-dialog.tsx
@@ -1,0 +1,22 @@
+import Dialog from '@mui/material/Dialog'
+import DialogContent from '@mui/material/DialogContent'
+import DialogTitle from '@mui/material/DialogTitle'
+
+import UserForm from '@/app/(auth)/systems/users/[uuid]/_parts/user-form'
+
+import useFormData from '@/providers/FormData'
+
+export default function UserFormDialog() {
+    const { isDataNotUndefined, isNew } = useFormData()
+
+    const text = isNew ? 'Buat akun baru' : 'Perbarui data akun'
+
+    return (
+        <Dialog maxWidth="xs" open={isDataNotUndefined}>
+            <DialogTitle>{text}</DialogTitle>
+            <DialogContent>
+                <UserForm />
+            </DialogContent>
+        </Dialog>
+    )
+}