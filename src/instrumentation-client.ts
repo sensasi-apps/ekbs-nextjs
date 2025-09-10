d2ef3112d00a12abf1e6436ff87b40f7f2a4bef2
Zain🍕 <zainadam.id@gmail.com>
2025-09-11 00:27:07 +0800

    fix lint error

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -1,29 +1,29 @@
 import * as Sentry from '@sentry/nextjs'
 
-    Sentry.init({
-        // Setting this option to true will print useful information to the console while you're setting up Sentry.
-        debug: false,
+Sentry.init({
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-        dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
-        // Replay may only be enabled for the client-side
-        integrations: [Sentry.replayIntegration()],
+    // Replay may only be enabled for the client-side
+    integrations: [Sentry.replayIntegration()],
 
-        // Set tracesSampleRate to 1.0 to capture 100%
-        // of transactions for performance monitoring.
-        // We recommend adjusting this value in production
-        tracesSampleRate: 1.0,
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
 
-        // Capture Replay for 10% of all sessions,
-        // plus for 100% of sessions with an error
-        // replaysSessionSampleRate: 0.1,
-        replaysOnErrorSampleRate: 1.0,
+    // Capture Replay for 10% of all sessions,
+    // plus for 100% of sessions with an error
+    // replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
 
-        // ...
+    // ...
 
-        // Note: if you want to override the automatic release value, do not set a
-        // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-        // that it will also get attached to your source maps
-    })
+    // Note: if you want to override the automatic release value, do not set a
+    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+    // that it will also get attached to your source maps
+})
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
