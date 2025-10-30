72128d20416903dc1bad07d86e2323523ca2e155
Zain🍕 <zainadam.id@gmail.com>
2025-10-30 21:39:55 +0800

    Limits snackbar maximum width
    Ensures snackbar notifications maintain a reasonable maximum width. This prevents them from stretching excessively on larger screens, improving readability and overall UI aesthetics.

--- src/providers/_parts/snackbar-provider.tsx
+++ src/providers/_parts/snackbar-provider.tsx
@@ -20,6 +20,9 @@ export default function SnackbarProvider() {
                 vertical: 'top',
             }}
             maxSnack={7}
+            style={{
+                maxWidth: 350,
+            }}
         />
     )
 }
