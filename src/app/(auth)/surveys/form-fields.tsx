f94183299aaeddbbe893cc95ec76f2e104c540c1
Zain🍕 <zainadam.id@gmail.com>
2025-11-27 00:33:43 +0800

    Adds initial survey management page
    Introduces a new page for managing surveys. This includes a data table for listing surveys, a dialog for creating and editing survey details, and basic form fields for survey name. Also sets up page title and metadata.

---
+++ src/app/(auth)/surveys/form-fields.tsx
@@ -0,0 +1,9 @@
+import TextField from '@/components/formik-fields/text-field'
+
+export default function FormFields() {
+    return (
+        <>
+            <TextField label="Nama" name="name" />
+        </>
+    )
+}
