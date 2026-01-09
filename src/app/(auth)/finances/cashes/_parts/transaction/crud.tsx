df99fda7fc048fbb0b92acb4df0fa5d2eb27363f
Zain🍕 <zainadam.id@gmail.com>
2026-01-09 19:16:49 +0800

    Improves transaction datatable filtering
    Updates `mui-datatable-delight` to version 2.0.2, including related MUI dependencies, to leverage the latest features and fixes.
    
    Enhances the transaction datatable by enabling advanced server-side filtering. Dynamic filter options are now provided for 'Kas' and 'Wallet' columns, allowing users to filter transactions more effectively.
    
    Addresses a minor issue in the transaction form by correctly checking for the presence of user activity logs before rendering the component.

--- src/app/(auth)/finances/cashes/_parts/transaction/crud.tsx
+++ src/app/(auth)/finances/cashes/_parts/transaction/crud.tsx
@@ -8,18 +8,18 @@ import dayjs from 'dayjs'
 // vendors
 import { Formik } from 'formik'
 import { type MutableRefObject, useCallback, useRef, useState } from 'react'
-import type {
-    DataTableProps,
-    GetRowDataType,
-    OnRowClickType,
-} from '@/components/data-table'
+import useSWR from 'swr'
+import type { GetRowDataType, OnRowClickType } from '@/components/data-table'
 // components
 import Datatable, { getNoWrapCellProps, mutate } from '@/components/data-table'
 import DialogWithTitle from '@/components/dialog-with-title'
 import Fab from '@/components/fab'
 import axios from '@/lib/axios'
+import TransactionTag from '@/modules/transaction/enums/transaction-tag'
 import type TransactionORM from '@/modules/transaction/types/orms/transaction'
 import type { Ymd } from '@/types/date-string'
+import type BusinessUnitCashORM from '@/types/orms/business-unit-cash'
+import type CashORM from '@/types/orms/cash'
 import formatNumber from '@/utils/format-number'
 // utils
 import errorCatcher from '@/utils/handle-422'
@@ -41,6 +41,10 @@ type CustomTx = TransactionORM & {
 let getRowDataRefGlobal: MutableRefObject<GetRowDataType<CustomTx> | undefined>
 
 export default function TransactionCrud() {
+    const { data: cashes } = useSWR<CashORM[]>('/data/cashes')
+    const { data: businessCashes } = useSWR<BusinessUnitCashORM[]>(
+        '/data/business-unit-cashes',
+    )
     const [values, setValues] = useState<FormValuesType>()
     const [status, setStatus] = useState<TransactionORM>()
     const getRowDataRef = useRef<GetRowDataType<CustomTx> | undefined>(
@@ -68,9 +72,165 @@ export default function TransactionCrud() {
         <>
             <Datatable<CustomTx>
                 apiUrl="/transactions/datatable"
-                columns={DATATABLE_COLUMNS}
+                columns={[
+                    {
+                        label: 'UUID',
+                        name: 'uuid',
+                        options: {
+                            display: 'excluded',
+                            filter: false,
+                        },
+                    },
+                    {
+                        label: 'Kode',
+                        name: 'short_uuid',
+                        options: {
+                            filter: false,
+                            searchable: false,
+                            sort: false,
+                        },
+                    },
+                    {
+                        label: 'TGL',
+                        name: 'at',
+                        options: {
+                            customBodyRender: toDmy,
+                            filter: false,
+                            setCellProps: getNoWrapCellProps,
+                        },
+                    },
+
+                    {
+                        label: 'Kas',
+                        name: 'cash_name',
+                        options: {
+                            customBodyRenderLite: dataIndex => {
+                                const data =
+                                    getRowDataRefGlobal.current?.(dataIndex)
+                                const chipColor =
+                                    (data?.amount ?? 0) > 0
+                                        ? 'success'
+                                        : 'error'
+
+                                if (!data) return ''
+
+                                return (
+                                    <Chip
+                                        color={chipColor}
+                                        label={data?.cash_name}
+                                        size="small"
+                                    />
+                                )
+                            },
+                            filterOptions: {
+                                names: cashes?.map(cash => cash.name).sort(),
+                            },
+                        },
+                    },
+                    {
+                        label: 'Wallet',
+                        name: 'wallet_name',
+                        options: {
+                            customBodyRenderLite: dataIndex => {
+                                const data =
+                                    getRowDataRefGlobal.current?.(dataIndex)
+
+                                return data?.wallet_name ? (
+                                    <Chip
+                                        color={data?.wallet_chip_color}
+                                        label={data?.wallet_name}
+                                        size="small"
+                                    />
+                                ) : (
+                                    ''
+                                )
+                            },
+                            filterOptions: {
+                                names: businessCashes
+                                    ?.map(
+                                        businessCash =>
+                                            businessCash.business_unit?.name ??
+                                            '',
+                                    )
+                                    .filter(name => !!name)
+                                    .sort(),
+                            },
+                            sort: false,
+                        },
+                    },
+                    {
+                        label: 'Akun',
+                        name: 'tag_names',
+                        options: {
+                            customBodyRenderLite: dataIndex => {
+                                return getRowDataRefGlobal
+                                    .current?.(dataIndex)
+                                    ?.['tag_names']?.split(', ')
+                                    .map(tagName => (
+                                        <Chip
+                                            key={tagName}
+                                            label={tagName}
+                                            size="small"
+                                        />
+                                    ))
+                                    .sort()
+                            },
+                            filterOptions: {
+                                names: Object.values(TransactionTag).sort(),
+                            },
+                            sort: false,
+                        },
+                    },
+                    {
+                        label: 'Nilai (Rp)',
+                        name: 'amount',
+                        options: {
+                            customBodyRenderLite: dataIndex => {
+                                const value =
+                                    getRowDataRefGlobal.current?.(
+                                        dataIndex,
+                                    )?.amount
+
+                                return (
+                                    <span
+                                        style={{
+                                            color:
+                                                typeof value === 'number' &&
+                                                value <= 0
+                                                    ? 'inherit'
+                                                    : green[500],
+                                            textAlign: 'right',
+                                        }}>
+                                        {formatNumber(value ?? 0)}
+                                    </span>
+                                )
+                            },
+                            filter: false,
+                        },
+                    },
+                    {
+                        label: 'Perihal',
+                        name: 'desc',
+                        options: {
+                            filter: false,
+                            sort: false,
+                        },
+                    },
+                    {
+                        label: 'Oleh',
+                        name: 'userActivityLogs.user.name',
+                        options: {
+                            customBodyRenderLite: (_, rowIndex) =>
+                                getRowDataRefGlobal.current?.(rowIndex)
+                                    ?.user_activity_logs?.[0]?.user.name,
+                            filter: false,
+                            sort: false,
+                        },
+                    },
+                ]}
                 defaultSortOrder={{ direction: 'desc', name: 'at' }}
                 download
+                filter
                 getRowDataCallback={fn => (getRowDataRef.current = fn)}
                 onRowClick={handleRowClick}
                 tableId="transaction-datatable"
@@ -116,124 +276,3 @@ export default function TransactionCrud() {
         </>
     )
 }
-
-const DATATABLE_COLUMNS: DataTableProps<CustomTx>['columns'] = [
-    {
-        label: 'UUID',
-        name: 'uuid',
-        options: {
-            display: 'excluded',
-        },
-    },
-    {
-        label: 'Kode',
-        name: 'short_uuid',
-        options: {
-            searchable: false,
-            sort: false,
-        },
-    },
-    {
-        label: 'TGL',
-        name: 'at',
-        options: {
-            customBodyRender: toDmy,
-            setCellProps: getNoWrapCellProps,
-        },
-    },
-
-    {
-        label: 'Kas',
-        name: 'cash_name',
-        options: {
-            customBodyRenderLite: dataIndex => {
-                const data = getRowDataRefGlobal.current?.(dataIndex)
-                const chipColor = (data?.amount ?? 0) > 0 ? 'success' : 'error'
-
-                if (!data) return ''
-
-                return (
-                    <Chip
-                        color={chipColor}
-                        label={data?.cash_name}
-                        size="small"
-                    />
-                )
-            },
-        },
-    },
-    {
-        label: 'Wallet',
-        name: 'wallet_name',
-        options: {
-            customBodyRenderLite: dataIndex => {
-                const data = getRowDataRefGlobal.current?.(dataIndex)
-
-                return data?.wallet_name ? (
-                    <Chip
-                        color={data?.wallet_chip_color}
-                        label={data?.wallet_name}
-                        size="small"
-                    />
-                ) : (
-                    ''
-                )
-            },
-            sort: false,
-        },
-    },
-    {
-        label: 'Akun',
-        name: 'tag_names',
-        options: {
-            customBodyRenderLite: dataIndex => {
-                return getRowDataRefGlobal
-                    .current?.(dataIndex)
-                    ?.['tag_names']?.split(', ')
-                    .map(tagName => (
-                        <Chip key={tagName} label={tagName} size="small" />
-                    ))
-            },
-            sort: false,
-        },
-    },
-    {
-        label: 'Nilai (Rp)',
-        name: 'amount',
-        options: {
-            customBodyRenderLite: dataIndex => {
-                const value = getRowDataRefGlobal.current?.(dataIndex)?.amount
-
-                return (
-                    <span
-                        style={{
-                            color:
-                                typeof value === 'number' && value <= 0
-                                    ? 'inherit'
-                                    : green[500],
-                            textAlign: 'right',
-                        }}>
-                        {formatNumber(value ?? 0)}
-                    </span>
-                )
-            },
-        },
-    },
-    {
-        label: 'Perihal',
-        name: 'desc',
-        options: {
-            sort: false,
-        },
-    },
-    {
-        label: 'Oleh',
-        name: 'userActivityLogs.user.name',
-        options: {
-            customBodyRenderLite: (_, rowIndex) =>
-                getRowDataRefGlobal.current?.(rowIndex)?.user_activity_logs?.[0]
-                    ?.user.name,
-            sort: false,
-        },
-    },
-]
