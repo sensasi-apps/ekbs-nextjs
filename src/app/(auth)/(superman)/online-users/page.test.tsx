17a187f665bfd2ed34c2b3d2291fc227d82c635c
🍕 <zainadam.id@gmail.com>
2026-09-18 16:32:18 +0800

    refactor: remove online users page and related tests from the project

--- src/app/(auth)/(superman)/online-users/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react'
-import { test } from 'vitest'
-import '@/test-utils/mock-setup'
-import Page from './page'
-
-test('Page renders without crashing', () => {
-    render(<Page />)
-})
--- src/components/auth-layout/_parts/nav-bar/NAV_ITEM_GROUPS/supermans.ts
+++ src/components/auth-layout/_parts/nav-bar/NAV_ITEM_GROUPS/supermans.ts
@@ -2,7 +2,6 @@
 import Biotech from '@mui/icons-material/Biotech'
 import Group from '@mui/icons-material/Group'
 import Note from '@mui/icons-material/Note'
-import OnlinePrediction from '@mui/icons-material/OnlinePrediction'
 import PrintIcon from '@mui/icons-material/Print'
 import SupervisedUserCircle from '@mui/icons-material/SupervisedUserCircle'
 // enums
@@ -40,13 +39,6 @@ export const supermans: NavItemGroup = {
             href: '/print-test',
             icon: PrintIcon,
             label: 'Test Cetak',
-        },
-
-        {
-            forRole: Role.SUPERMAN,
-            href: '/online-users',
-            icon: OnlinePrediction,
-            label: 'Pengguna Online',
         },
     ],
     label: 'Superman',
