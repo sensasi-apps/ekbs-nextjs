de30f41ad20e5a66167249737d84c3d70abac936
🍕 <zainadam.id@gmail.com>
2026-09-18 16:02:00 +0800

    feat: add user search navigation item to systems menu

--- src/components/auth-layout/_parts/nav-bar/NAV_ITEM_GROUPS/systems.ts
+++ src/components/auth-layout/_parts/nav-bar/NAV_ITEM_GROUPS/systems.ts
@@ -2,8 +2,10 @@
 
 // icons-materials
 import GroupIcon from '@mui/icons-material/Group'
+import ManageSearchIcon from '@mui/icons-material/ManageSearch'
 // import SettingsIcon from '@mui/icons-material/Settings'
 // enums
+import PalmBunch from '@/enums/permissions/PalmBunch'
 import Role from '@/enums/role'
 import type NavItemGroup from '../types/nav-item-group'
 
@@ -14,6 +16,12 @@ export const systemsNavItemGroup: NavItemGroup = {
             href: '/systems/users',
             icon: GroupIcon,
             label: 'Pengguna',
+        },
+        {
+            forPermission: PalmBunch.SEARCH_USER,
+            href: `${process.env.NEXT_PUBLIC_V2_DOMAIN}/systems/user-search`,
+            icon: ManageSearchIcon,
+            label: 'Cari Pengguna',
         },
         // {
         //     href: '/systems/settings',
